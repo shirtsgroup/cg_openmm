aaa1f431caa8017d6a18b19cc20cdf5ffb154eda
root <root@crestone.int.colorado>
2019-02-13 13:18:04 -0700

    Adding new CG model tests

--- yank_REMD/CG_octamer/run-CG_octamer-implicit.sh
+++ yank_REMD/CG_octamer/run-CG_octamer-implicit.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
 #
-# p-xylene binding to T4 lysozyme L99A example run script (serial mode)
-#
 
+gromacs_rtp="/home/gmeek/software/anaconda3/pkgs/gromacs-4.6.5-0/share/gromacs/top/gromos54a7.ff/aminoacids.rtp"
+
+gromacs_atp="/home/gmeek/software/anaconda3/pkgs/gromacs-4.6.5-0/share/gromacs/top/gromos54a7.ff/atomtypes.atp"
+
+
+# echo "" >> $gromacs_rtp
+# echo "[ CG ]" >> $gromacs_rtp
+# echo " [ atoms ]" >> $gromacs_rtp
+# echo "   CG1  CH2     0.00000     0" >> $gromacs_rtp
+# echo " [ bonds ]" >> $gromacs_rtp
+# echo " [ angles ]" >> $gromacs_rtp
+# echo ";  ai    aj    ak   gromos type" >> $gromacs_rtp
+# echo " [ impropers ]" >> $gromacs_rtp
+# echo ";  ai    aj    ak    al   gromos type" >> $gromacs_rtp
+# echo " [ dihedrals ]" >> $gromacs_rtp
+# echo ";  ai    aj    ak    al   gromos type" >> $gromacs_rtp
+
+# echo "" >> $gromacs_atp
+# echo "  CG1  12.001 ;     Coarse-grained particle type 1" >> $gromacs_atp
+# echo "  CG2  12.001 ;     Coarse-grained particle type 2" >> $gromacs_atp
+#fi
+
+#exit
 # Run the simulation
 echo "Running simulation..."
 yank script --yaml=CG_octamer.yaml
