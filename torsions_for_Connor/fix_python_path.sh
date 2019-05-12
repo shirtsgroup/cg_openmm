2550982f89bb196c6a3d153a9528c9007b8b73c7
garrettameek <garrett.a.meek@gmail.com>
2019-05-12 14:26:51 -0600

    Reorganized the head directory.
    Former-commit-id: 6102a65f36da15170788bac6c5d6a26564784a78 [formerly 10d9dfefda54473a95e78bf238dad1d9f21b7f2b]
    Former-commit-id: 7f8adc18f17ff6cbe189ea3c191b76c8eadafc00
    Former-commit-id: 5b87f6de5fc55ae998b890db2ed4357c20f7be76 [formerly da201d2d967c91c6f887a007552ecb38858f2021]
    Former-commit-id: 8b182fe8fb351e9d2e048685e5109be3a47c177a
    Former-commit-id: 0bfe209314f05cc39e0d322094b4546fd910ddc8 [formerly e6afc647d7856ed9b90e0764281755409263c354]
    Former-commit-id: 600f6efdf6a2ce66a0e0db67d9c98b6db14a28b9

---
+++ torsions_for_Connor/fix_python_path.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This script is intended to fix the python path for Connor's laptop,
+# so that it points to the 'foldamers' and 'cg_openmm' repositories.
+
+cd /mnt/c/Users/thund/GitHub
