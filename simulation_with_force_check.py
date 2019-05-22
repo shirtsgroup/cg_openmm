aad2259c630e0dfadb8bd66af969d63cccf49021
garrettameek <garrett.a.meek@gmail.com>
2019-05-22 11:09:01 -0600

    Updates to examples and CGModel structure/attributes.
    Former-commit-id: 21b3f8df9f1fded4106c8a575fd134651f16dd10 [formerly c83da0c40a4f678223dda217145a762bea253c5f]
    Former-commit-id: 7a194ab0b0063dd3947586aad1e94b61ba24eb44
    Former-commit-id: 593aded8d95367a268952bc7d859a5cd2c82eec8 [formerly aaae551ea628d593ac823e9dfd4801c1ba07f917]
    Former-commit-id: 1c3bb776921277984fbf97f8745534b4466bbc29
    Former-commit-id: 3545a6ec1e8339bdf3463810c6312ad41c03a34e [formerly f096686863cf785b6e8657b10a25f113379bb6f6]
    Former-commit-id: fc7862fa1edc6bf66d756145ae062f419c516fec

--- simulation_with_force_check.py
+++ simulation_with_force_check.py
@@ -17,7 +17,11 @@ print("Found "+str(cpu_count)+" processors on the system.")
 print("Using "+str(processors)+" processors for this calculation.")
 
 # OpenMM simulation settings
-total_simulations = processors
+number_of_simulations = processors
+pdb_mm_obj = PDBFile('init_0.pdb')
+positions = pdb_mm_obj.getPositions()
+output_pdb = "test_simulation.pdb"
+output_data = "test_simulation.dat"
 box_size = 10.00 * unit.nanometer # box width
 cutoff = box_size / 2.0 * 0.99
 simulation_time_step = 0.1 * unit.femtosecond # Units = picoseconds
@@ -98,21 +102,116 @@ if len(torsion_force_constants) != 0: include_torsion_forces = True
 include_bond_angle_forces = False
 if max_force != None: check_energy_conservation = True
 
-for simulation_index in range(total_simulations):
-  input_coordinates = str("init_"+str(simulation_index)+".pdb")
-# Read coordinates and build a coarse-grained model
-  pdb_mm_obj = PDBFile(input_coordinates)
+if positions != None:
+ cgmodel = CGModel(positions=positions,polymer_length=polymer_length,backbone_lengths=backbone_lengths, sidechain_lengths=sidechain_lengths, sidechain_positions = sidechain_positions, masses = masses, sigmas = sigmas, epsilons = epsilons, bond_lengths = bond_lengths, bond_force_constants = bond_force_constants, torsion_force_constants=torsion_force_constants, equil_dihedral_angle=equil_dihedral_angle,bond_angle_force_constants=bond_angle_force_constants, charges = charges,constrain_bonds=constrain_bonds,include_bond_forces=include_bond_forces,include_nonbonded_forces=include_nonbonded_forces,include_bond_angle_forces=include_bond_angle_forces,include_torsion_forces=include_torsion_forces,check_energy_conservation=False)
+
+# Build an OpenMM simulation object
+ simulation = build_mm_simulation(cgmodel.topology,cgmodel.system,cgmodel.positions,temperature=temperature,simulation_time_step=simulation_time_step,total_simulation_time=total_simulation_time,output_pdb=output_pdb,output_data=output_data,print_frequency=print_frequency)
+
+ simulation.context.setPositions(positions=cgmodel.positions)
+ simulation.context.applyConstraints(0.005)
+ state = simulation.context.getState(getForces=True,getPositions=True,getVelocities=True)
+ forces = state.getForces()
+ print("The forces for this model are:")
+ print(forces)
+ print("The non-bonded interactions are:")
+ print(cgmodel.nonbonded_interaction_list)
+# exit()
+ print("The non-bonded interaction distances are:")
+ print([distance.in_units_of(unit.angstrom)._value for distance in distances(cgmodel.nonbonded_interaction_list,cgmodel.positions)])
+else:
+ while not cgmodel_built and attempt < max_build_attempts:
+# Build a coarse grained model
+  pdb_mm_obj = PDBFile('test-1-1.pdb')
   positions = pdb_mm_obj.getPositions()
   cgmodel = CGModel(positions=positions,polymer_length=polymer_length,backbone_lengths=backbone_lengths, sidechain_lengths=sidechain_lengths, sidechain_positions = sidechain_positions, masses = masses, sigmas = sigmas, epsilons = epsilons, bond_lengths = bond_lengths, bond_force_constants = bond_force_constants, torsion_force_constants=torsion_force_constants, equil_dihedral_angle=equil_dihedral_angle,bond_angle_force_constants=bond_angle_force_constants, charges = charges,constrain_bonds=constrain_bonds,include_bond_forces=include_bond_forces,include_nonbonded_forces=include_nonbonded_forces,include_bond_angle_forces=include_bond_angle_forces,include_torsion_forces=include_torsion_forces,check_energy_conservation=False)
 
-  output_pdb = str("simulation_"+str(simulation_index)+".pdb")
-  output_data = str("simulation_"+str(simulation_index)+".dat")
+  positions = cgmodel.positions
+# print(positions)
+# exit()
+# Write a PDB file so that we can build a topology
+  pdb_file = "test.pdb"
+  write_pdbfile_without_topology(cgmodel,pdb_file)
+
+# Build a topology using the PDB file as input
+  pdb_mm_obj = PDBFile(pdb_file)
+  cgmodel.topology = pdb_mm_obj.getTopology()
+
 # Build an OpenMM simulation object
   simulation = build_mm_simulation(cgmodel.topology,cgmodel.system,cgmodel.positions,temperature=temperature,simulation_time_step=simulation_time_step,total_simulation_time=total_simulation_time,output_pdb=output_pdb,output_data=output_data,print_frequency=print_frequency)
 
-  total_steps = round(total_simulation_time.__div__(simulation_time_step))
-#print(total_steps)
-  simulation.step(total_steps)
-# print("Finished simulation.")
+# if constrain_bonds: simulation.context.applyConstraints(unit.Quantity(0.1,unit.angstrom))
+# Test the construction of our simulation object
 
+  simulation.context.setPositions(positions=cgmodel.positions)
+  simulation.context.applyConstraints(0.005)
+  state = simulation.context.getState(getForces=True,getPositions=True,getVelocities=True)
+  forces = state.getForces()
+  force_pass_list = []
+  force_list = []
+  force_comparison = max_force
+  for force in forces:
+   for direction in force:
+    force_pass_list.append(direction._value < force_comparison)
+    force_list.append(direction)
+    if str(direction._value) == 'nan':
+     print("Error: getting 'nan' for the forces on particles.")
+     print("The forces are: "+str(str([force_list[i]._value for i in range(len(force_list))])+" "+str(force_list[0].unit)))
+     print("The positions are: "+str(state.getPositions()))
+     print("The velocities are: "+str(state.getVelocities()))
+#    exit()
+  if all(force_pass_list): 
+   print("Successfully built a coarse-grained model")
+   print("with a maximum force of: "+str(max(force_list)))
+   cgmodel_built = True
+  else:
+   if largest_force == None:
+    largest_force = max(force_list)
+    best_positions = cgmodel.positions
+   else:
+    if max(force_list) < largest_force:
+     largest_force = max(force_list)
+     best_positions = cgmodel.positions
+
+  attempt = attempt + 1
+
+ if attempt == max_build_attempts:
+  positions = best_positions
+  print("Error: Exceeded the maximum number of model building attempts.")
+  print("Error: The forces are too large for the best structure to be a suitable initial configuration.")
+  print("Going to attempt a simulation using the best structure.")
+  print("The nonbonded interaction distances for particles in this configuration are:")
+  print(str([distance(positions[interaction[0]],positions[interaction[1]])._value for interaction in cgmodel.nonbonded_interactions])+" "+str(positions.unit))
+  print("The forces are: "+str(str([force_list[i]._value for i in range(len(force_list))])+" "+str(force_list[0].unit)))
+ else:
+  print("Successfully built a coarse grained model.")
+  print("Whose forces are: "+str(str([force_list[i]._value for i in range(len(force_list))])+" "+str(force_list[0].unit)))
+
+# Record the positions if we successfully generated a random configuration without clashes.
+ cgmodel.positions = best_positions
+ positions = cgmodel.positions
+ pdb_file = "init_coord.pdb"
+ write_cg_pdb(cgmodel,pdb_file)
+
+# Run the simulation
+total_steps = round(total_simulation_time.__div__(simulation_time_step))
+print(total_steps)
+simulation.step(total_steps)
+print("Finished simulation.")
+exit()
+
+# Confirm that the bond lengths were preserved
+pdb_mm_obj = PDBFile(output_pdb)
+num_frames = pdb_mm_obj.getNumFrames()
+bond_list = cgmodel.get_bond_list()
+for frame in range(num_frames):
+ positions = pdb_mm_obj.getPositions(frame=frame)
+ for bond in bond_list:
+  dist = distance(positions[bond[0]-1],positions[bond[1]-1]).in_units_of(unit.angstrom)._value
+  length = bond_length.in_units_of(unit.angstrom)._value
+  if dist < 0.9 * length or dist > 1.1 * length:
+    print("Error: the bond is breaking between particles")
+    print(str(bond[0])+" and "+str(bond[1])+" in frame "+str(frame))
+    print("The bond length is: "+str(dist))
+    exit()
 exit()
