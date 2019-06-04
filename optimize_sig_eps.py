41312f64dad912725336307f5ee71cb1068053f7
garrettameek <garrett.a.meek@gmail.com>
2019-06-04 09:50:10 -0600

    Updated epsilon-sigma parameter sampling example.
    Former-commit-id: 5616e32b447e809e39f8bce535d71bf3f1b0043c [formerly 74aaf433974719b074aff6524117d0006268d266]
    Former-commit-id: 623fc44ef0e9c85a6d4941ac4d9a5aa94043d178
    Former-commit-id: e41c592731f5ad15dbc673be2c0cc9044cfecd3e [formerly ffacf531ded6df1ac4799ba4f5d7fddcf1a733db]
    Former-commit-id: d9741714f05328b83857ea96e6ed20d28448a58a
    Former-commit-id: e4196f2b5b0f02704f8308091f7b3ae71af5abdc [formerly faea813535dae5b40ae2fb209dc5f0248b868ab8]
    Former-commit-id: e82857153e77f9341b66700e9b4b667c37ff0693

--- optimize_sig_eps.py
+++ optimize_sig_eps.py
@@ -6,15 +6,17 @@ import matplotlib.pyplot as pyplot
 import mdtraj as md
 from simtk import unit
 # foldamers utilities
-from foldamers.src.cg_model.cgmodel import CGModel
+from foldamers.src.cg_model.cgmodel import basic_cgmodel
+from foldamers.src.utilities.util import random_positions
 # cg_openmm utilities
 from cg_openmm.src.build.cg_build import build_mm_simulation
 from cg_openmm.src.utilities.iotools import read_mm_energies
 
-simulation_time_step = 0.1 * unit.femtosecond
+simulation_time_step = 0.01 * unit.femtosecond
 temperature = 300.0 * unit.kelvin
 print_frequency = 20 # Number of steps to skip when printing output
-total_steps = 10
+total_steps = 1000
+total_simulation_time = total_steps * simulation_time_step
 
 # Define static model settings
 backbone_length = 1 # Number of backbone beads
@@ -27,32 +29,37 @@ bond_length = 1.0 * unit.angstrom # bond length
 # Set variable model settings
 base_sigma = 2.4 * unit.angstrom # Lennard-Jones interaction distance
 base_epsilon = 0.5 * unit.kilocalorie_per_mole # Lennard-Jones interaction strength
-sigma_list = [(base_sigma).__add__(i * base_sigma.unit) for i in range(-0.4,3.0,0.2)]
-epsilon_list = [(base_epsilon).__add__(i * base_epsilon.unit) for i in range(-0.2,5.0,0.2)
-sigma_epsilon_list = np.array(len(sigma_list),len(epsilon_list),dtype=float)
+sigma_list = [(base_sigma).__add__(i * base_sigma.unit) for i in [ j * 0.2 for j in range(-2,3,1)]]
+epsilon_list = [(base_epsilon).__add__(i * base_epsilon.unit) for i in [ j * 0.2 for j in range(-1,3,1)]]
+sigma_epsilon_list = np.zeros((len(sigma_list),len(epsilon_list)))
 
 for sigma_index in range(len(sigma_list)):
   for epsilon_index in range(len(epsilon_list)):
     sigma = sigma_list[sigma_index]
     epsilon = epsilon_list[epsilon_index]
+    print("Evaluating the energy for a model with:")
+    print("sigma="+str(sigma)+" and epsilon="+str(epsilon))
     # Build a coarse grained model
     cgmodel = basic_cgmodel(polymer_length=polymer_length, backbone_length=backbone_length, sidechain_length=sidechain_length, sidechain_positions=sidechain_positions, mass=mass, sigma=sigma, epsilon=epsilon, bond_length=bond_length)
     # Build a simulation object
     output_file = 'sig_eps_opt_temp.dat'
     simulation = build_mm_simulation(cgmodel.topology,cgmodel.system,cgmodel.positions,temperature=temperature,simulation_time_step=simulation_time_step,total_simulation_time=total_simulation_time,print_frequency=print_frequency,output_data=output_file)
     # Run the simulation
-    simulation.step(total_steps)
+    try:
+      simulation.step(total_steps)
+    except:
+      success = False
+      while not success:
+        cgmodel.positions = random_positions(cgmodel)
+        simulation = build_mm_simulation(cgmodel.topology,cgmodel.system,cgmodel.positions,temperature=temperature,simulation_time_step=simulation_time_step,total_simulation_time=total_simulation_time,print_frequency=print_frequency,output_data=output_file)
+        try:
+          simulation.step(total_steps)
+          success = True
+        except:
+          continue
     simulation_data = read_mm_energies(output_file)
+    minimum_energy = min(simulation_data['potential_energy'])
+    sigma_epsilon_list[sigma_index][epsilon_index] = minimum_energy
 
-figure = pyplot.figure(0)
-colors = pyplot.cm.rainbow(np.linspace(0, 1, len(dihedral_list)))
-for dihedral, c in zip(dihedrals, colors):
- pyplot.scatter(dihedral, energies, color=c, figure=figure)
-pyplot.xlabel("Dihedral Angle (Degrees)")
-pyplot.ylabel("Potential Energy (kJ/mol)")
-pyplot.title("Dihedral distribution data for simulation of 1,1-CG model")
-pyplot.savefig("/CG-11-distribution.png")
-pyplot.legend(dihedral_list)
-pyplot.show()
-pyplot.close()
+print(sigma_epsilon_list)
 exit()
