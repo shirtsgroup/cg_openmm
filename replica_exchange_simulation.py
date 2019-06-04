e6b57805a2f607a734cb3c903f63241aa85f4eb2
garrettameek <garrett.a.meek@gmail.com>
2019-06-03 18:58:52 -0600

    Added new replica exchange example, with output.
    Former-commit-id: 1290b6d88e49ac4eea7dc928da0760cafec89d8e [formerly ba96cff0a78f4e3c7ac8d0928db8c4b6202ef792]
    Former-commit-id: 529af91d2540b34e0a0b4cf2cdd3f4c0766cdb5d
    Former-commit-id: bfec700676ed45dcdc6e47b9f73c54117138f5b5 [formerly aea19116928190a5120ccd7903fda23d5a6f087a]
    Former-commit-id: f0f29cc58c6de5917852cffe0efcec4bf9a7e1db
    Former-commit-id: 5f0e8d235616cb1be1051cc40c4a1cf8ee611ae8 [formerly 3abae20ffe04adb3ca984cf84ee572a40f54689d]
    Former-commit-id: 697e76dd900998414ec09d94689d191daf4781ab

--- replica_exchange_simulation.py
+++ replica_exchange_simulation.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 
-import os
 from simtk import unit
 from foldamers.src.cg_model.cgmodel import basic_cgmodel
 from cg_openmm.src.build.cg_build import build_mm_simulation
+from cg_openmm.src.simulation.rep_exch import replica_exchange, get_replica_energies
 
 # OpenMM simulation settings
-temperature = 300.0 * unit.kelvin
+simulation_time_step = 0.01 * unit.femtosecond
 print_frequency = 20 # Number of steps to skip when printing output
 total_simulation_time = 1.0 * unit.picosecond # Units = picoseconds
+
+# Yank (replica exchange) simulation settings
+temperature_list = [(300.0 * unit.kelvin).__add__(i * unit.kelvin) for i in range(0,30,10)]
 
 # Coarse grained model settings
 polymer_length=8
@@ -24,12 +27,12 @@ epsilon = unit.Quantity(0.5,unit.kilocalorie_per_mole)
 cgmodel = basic_cgmodel(polymer_length=polymer_length,backbone_length=backbone_length,sidechain_length=sidechain_length,sidechain_positions=sidechain_positions,mass=mass,bond_length=bond_length,sigma=sigma,epsilon=epsilon)
 
 # Build an OpenMM simulation object
-if not os.path.exists('basic_simulation'): os.mkdir('basic_simulation')
-output_pdb = 'basic_simulation/basic_simulation.pdb'
-output_data = 'basic_simulation/basic_simulation.dat'
-simulation = build_mm_simulation(cgmodel.topology,cgmodel.system,cgmodel.positions,temperature=temperature,output_pdb=output_pdb,output_data=output_data,print_frequency=print_frequency)
+#simulation = build_mm_simulation(cgmodel.topology,cgmodel.system,cgmodel.positions,temperature=temperature,output_pdb=output_pdb,output_data=output_data,print_frequency=print_frequency)
 
-# Run simulation
-simulation.step(500)
+# Run a replica exchange simulation with this cgmodel
+replica_energies = replica_exchange(cgmodel.topology,cgmodel.system,cgmodel.positions,temperature_list=temperature_list,simulation_time_step=simulation_time_step,total_simulation_time=total_simulation_time,print_frequency=print_frequency)
+
+# Process and analyze the replica exchange simulation data
+print(replica_energies)
 
 exit()
