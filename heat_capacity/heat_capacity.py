54c1c2c3a689c88d1a9316c3907f03e427d48c21
Garrett <garrett.a.meek@gmail.com>
2019-08-05 01:20:24 -0600

    Reorganized examples, and added new heat capacity data.
    Former-commit-id: 429a3ad3edb7ccc1b930a2194f7506b83a623ced
    Former-commit-id: b49dbe94ddd7a2e01aecc62879b36bc200e94674
    Former-commit-id: 833ebcb7059030d6ffb05f9c61473704b86f39c1 [formerly 4cfdbbe4dc32867751264a0acf22f0b3f5e43d2f]
    Former-commit-id: b4a8e7743350d840a0f75f03d2f97662c867c8a5
    Former-commit-id: 8d73692af7e0bd2ed4a582054e5a1387fe40bd69 [formerly 772ab20c69a9071c5d632fb561e154eb126a053d]
    Former-commit-id: d4d8f43c4945541ece795f4120f8570dce9f80ad

--- heat_capacity/heat_capacity.py
+++ heat_capacity/heat_capacity.py
@@ -12,17 +12,17 @@ from cg_openmm.src.simulation.rep_exch import *
 top_directory = 'output'
 if not os.path.exists(top_directory):
   os.mkdir(top_directory)
-run_simulations = False
+run_simulations = True
 
 # OpenMM simulation settings
 print_frequency = 5 # Number of steps to skip when printing output
-total_simulation_time = 1.0 * unit.picosecond # Units = picoseconds
+total_simulation_time = 10.0 * unit.picosecond # Units = picoseconds
 simulation_time_step = 5.0 * unit.femtosecond
 total_steps = round(total_simulation_time.__div__(simulation_time_step))
 
 # Yank (replica exchange) simulation settings
 output_data=str(str(top_directory)+"/output.nc")
-number_replicas = 45
+number_replicas = 100
 temperature_increment = 5 # unit.kelvin
 temperature_list = [(150.0 * unit.kelvin).__add__(i * unit.kelvin) for i in range(0,number_replicas*temperature_increment,temperature_increment)]
 if total_steps > 10000:
@@ -73,46 +73,55 @@ torsion_force_constants = {'bb_bb_bb_bb_torsion_k': torsion_force_constant,'bb_b
 equil_torsion_angle = 0
 equil_torsion_angles = {'bb_bb_bb_bb_torsion_0': equil_torsion_angle,'bb_bb_bb_sc_torsion_0': equil_torsion_angle,'bb_bb_sc_sc_torsion_0': equil_torsion_angle, 'bb_sc_sc_sc_torsion_0': equil_torsion_angle, 'sc_bb_bb_sc_torsion_0': equil_torsion_angle, 'bb_sc_sc_bb_torsion_0': equil_torsion_angle, 'sc_sc_sc_sc_torsion_0': equil_torsion_angle, 'sc_bb_bb_bb_torsion_0': equil_torsion_angle}
 
-cgmodel = CGModel(polymer_length=polymer_length,backbone_lengths=backbone_lengths,sidechain_lengths=sidechain_lengths,sidechain_positions=sidechain_positions,masses=masses,sigmas=sigmas,epsilons=epsilons,bond_lengths=bond_lengths,bond_force_constants=bond_force_constants,bond_angle_force_constants=bond_angle_force_constants,torsion_force_constants=torsion_force_constants,equil_bond_angles=equil_bond_angles,equil_torsion_angles=equil_torsion_angles,include_nonbonded_forces=include_nonbonded_forces,include_bond_forces=include_bond_forces,include_bond_angle_forces=include_bond_angle_forces,include_torsion_forces=include_torsion_forces,constrain_bonds=constrain_bonds)
+C_v_list = []
+dC_v_list = []
 
-cgmodel.topology = build_topology(cgmodel,use_pdbfile=True)
+sigma_range = range(round(bond_length._value*1.5),round(bond_length._value*2.5))
+for sigma in [ i * 1.0 * unit.angstrom for i in sigma_range]: 
+  print("Performing simulations and heat capacity analysis for a coarse grained model")
+  print("with sigma values of "+str(sigma))
+  sigmas = {'bb_bb_sigma': sigma,'bb_sc_sigma': sigma,'sc_sc_sigma': sigma}
+  cgmodel = CGModel(polymer_length=polymer_length,backbone_lengths=backbone_lengths,sidechain_lengths=sidechain_lengths,sidechain_positions=sidechain_positions,masses=masses,sigmas=sigmas,epsilons=epsilons,bond_lengths=bond_lengths,bond_force_constants=bond_force_constants,bond_angle_force_constants=bond_angle_force_constants,torsion_force_constants=torsion_force_constants,equil_bond_angles=equil_bond_angles,equil_torsion_angles=equil_torsion_angles,include_nonbonded_forces=include_nonbonded_forces,include_bond_forces=include_bond_forces,include_bond_angle_forces=include_bond_angle_forces,include_torsion_forces=include_torsion_forces,constrain_bonds=constrain_bonds)
 
-# Run a replica exchange simulation with this cgmodel
-if run_simulations:
-  replica_energies,replica_positions,replica_states = run_replica_exchange(cgmodel.topology,cgmodel.system,cgmodel.positions,temperature_list=temperature_list,simulation_time_step=simulation_time_step,total_simulation_time=total_simulation_time,print_frequency=print_frequency,output_data=output_data)
-else:
-  replica_energies,replica_positions,replica_states = read_replica_exchange_data(system=cgmodel.system,topology=cgmodel.topology,temperature_list=temperature_list,output_data=output_data,print_frequency=print_frequency)
+  cgmodel.topology = build_topology(cgmodel,use_pdbfile=True)
 
-steps_per_stage = round(total_steps/exchange_attempts)
+  # Run a replica exchange simulation with this cgmodel
+  if run_simulations:
+    replica_energies,replica_positions,replica_states = run_replica_exchange(cgmodel.topology,cgmodel.system,cgmodel.positions,temperature_list=temperature_list,simulation_time_step=simulation_time_step,total_simulation_time=total_simulation_time,print_frequency=print_frequency,output_data=output_data)
+  else:
+    replica_energies,replica_positions,replica_states = read_replica_exchange_data(system=cgmodel.system,topology=cgmodel.topology,temperature_list=temperature_list,output_data=output_data,print_frequency=print_frequency)
 
-minimum_energy_structure = get_minimum_energy_pose(cgmodel.topology,replica_energies,replica_positions)
+  steps_per_stage = round(total_steps/exchange_attempts)
+  plot_replica_exchange_energies(replica_energies,temperature_list,simulation_time_step,steps_per_stage=steps_per_stage)
+  plot_replica_exchange_summary(replica_states,temperature_list,simulation_time_step,steps_per_stage=steps_per_stage)
 
-plot_replica_exchange_energies(replica_energies,temperature_list,simulation_time_step,steps_per_stage=steps_per_stage)
+  num_intermediate_states = 1
+  mbar,E_kn,E_expect,dE_expect,new_temp_list = get_mbar_expectation(replica_energies,temperature_list,num_intermediate_states)
 
-plot_replica_exchange_summary(replica_states,temperature_list,simulation_time_step,steps_per_stage=steps_per_stage)
+  mbar,E_kn,DeltaE_expect,dDeltaE_expect,new_temp_list = get_mbar_expectation(E_kn,temperature_list,num_intermediate_states,mbar=mbar,output='differences')
 
-mbar,E_kln,E_expect,dE_expect = get_mbar_expectation(replica_energies,temperature_list,len(temperature_list))
+  mbar,E_kn,E2_expect,dE2_expect,new_temp_list = get_mbar_expectation(E_kn**2,temperature_list,num_intermediate_states,mbar=mbar)
 
-mbar,E_kln,DeltaE_expect,dDeltaE_expect = get_mbar_expectation(E_kln,temperature_list,len(temperature_list),output='differences')
+  df_ij,ddf_ij = get_free_energy_differences(mbar)
 
-mbar,E_kln,E2_expect,dE2_expect = get_mbar_expectation(E_kln**2,temperature_list,len(temperature_list))
 
-df_ij,ddf_ij = get_free_energy_differences(mbar)
-
-C_v,dC_v = calculate_heat_capacity(E_expect,E2_expect,temperature_list)
+  C_v,dC_v = calculate_heat_capacity(E_expect,E2_expect,dE_expect,DeltaE_expect,dDeltaE_expect,df_ij,ddf_ij,new_temp_list,len(temperature_list),num_intermediate_states)
+  C_v_list.append(C_v)
+  dC_v_list.append(dC_v)
 
 file_name = "heat_capacity.png"
 figure = pyplot.figure(1)
-temperatures = np.array([temperature for temperature in Temp_k])
-C_v = np.array([allCv_expect[i][0][0] for i in range(len(allCv_expect[:]))])
-print(C_v)
-print(temperatures)
-#C_v = np.array([allCv_expect[0][i][0] for i in range(len(allCV_expect[0]))])
-pyplot.plot(temperatures,C_v,figure=figure)
+temperatures = np.array([temperature._value for temperature in new_temp_list])
+legend_labels = [ str("$\sigma / r_{bond}$= "+str(round(i/bond_length._value,2)))  for i in sigma_range]
+
+for C_v in C_v_list:
+ C_v = np.array([C_v[i][0] for i in range(len(C_v))])
+ pyplot.plot(temperatures,C_v,figure=figure)
 
 pyplot.xlabel("Temperature ( Kelvin )")
 pyplot.ylabel("C$_v$ ( kcal/mol * Kelvin )")
-pyplot.title("Heat capacity")
+pyplot.title("Heat capacity for variable $\sigma / r_{bond}$")
+pyplot.legend(legend_labels)
 pyplot.savefig(file_name)
 pyplot.show()
 pyplot.close()
