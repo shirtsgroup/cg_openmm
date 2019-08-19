984205a903e904fc7971a4f3b183cb3d7d474cc0
Garrett <garrett.a.meek@gmail.com>
2019-08-19 00:38:55 -0600

    Updated examples, as well as the ensembles for a 12-mer.
    Former-commit-id: 51e892c833a0da3172c9059ac20b0aa4a9010b0f
    Former-commit-id: 11efc06ea5b3624245539a3a727d8d494c357f69
    Former-commit-id: 675f86764d3621ec88d177d4a7faf28f30395390 [formerly 2732d26a6bdc164a5898ac00bec17782312f34fc]
    Former-commit-id: 7fc40ae74cd542a06cffb5714a347534a58eeb57
    Former-commit-id: 466b4070436feb4ba4bc72c61926ed5aee214f87 [formerly 9ab7f9c24270b4036523caee9fc0a5351cd83d66]
    Former-commit-id: 749d2d4566ca46381ce6bf24f4c2c3142786ff98

---
+++ generate_ensembles/get_ensemble.py
@@ -0,0 +1,98 @@
+import os
+import numpy as np
+import matplotlib.pyplot as pyplot
+from simtk import unit
+from foldamers.src.cg_model.cgmodel import CGModel
+from foldamers.src.parameters.reweight import *
+from foldamers.src.thermo.calc import calculate_heat_capacity
+from foldamers.src.utilities.util import random_positions
+from cg_openmm.src.build.cg_build import build_topology
+from cg_openmm.src.simulation.rep_exch import *
+from foldamers.src.ensembles.ens_build import *
+
+grid_size = 1
+
+# Job settings
+top_directory = 'output'
+if not os.path.exists(top_directory):
+  os.mkdir(top_directory)
+run_simulations = True
+
+# OpenMM simulation settings
+print_frequency = 5 # Number of steps to skip when printing output
+total_simulation_time = 2.0 * unit.nanosecond # Units = picoseconds
+simulation_time_step = 5.0 * unit.femtosecond
+total_steps = round(total_simulation_time.__div__(simulation_time_step))
+
+# Yank (replica exchange) simulation settings
+output_data=str(str(top_directory)+"/output.nc")
+number_replicas = 20
+min_temp = 200.0 * unit.kelvin
+max_temp = 350.0 * unit.kelvin
+temperature_list = get_temperature_list(min_temp,max_temp,number_replicas)
+print("Using "+str(len(temperature_list))+" replicas.")
+if total_steps > 10000:
+   exchange_attempts = round(total_steps/1000)
+else:
+   exchange_attempts = 10
+
+###
+#
+# Coarse grained model settings
+#
+###
+
+# Global definitions
+polymer_length=12
+backbone_lengths=[1]
+sidechain_lengths=[1]
+sidechain_positions=[0]
+include_bond_forces=False
+include_bond_angle_forces=True
+include_nonbonded_forces=True
+include_torsion_forces=True
+constrain_bonds = True
+
+# Particle properties
+mass = 100.0 * unit.amu
+masses = {'backbone_bead_masses': mass, 'sidechain_bead_masses': mass}
+
+# Bonded interaction properties
+bond_length = 7.5 * unit.angstrom
+bond_lengths = {'bb_bb_bond_length': bond_length,'bb_sc_bond_length': bond_length,'sc_sc_bond_length': bond_length}
+bond_force_constant = 1250 * unit.kilojoule_per_mole / unit.nanometer / unit.nanometer
+bond_force_constants = {'bb_bb_bond_k': bond_force_constant, 'bb_sc_bond_k': bond_force_constant, 'sc_sc_bond_k': bond_force_constant}
+
+# Bond angle properties
+bond_angle_force_constant = 2000 * unit.kilojoule_per_mole / unit.radian / unit.radian
+bond_angle_force_constants = {'bb_bb_bb_angle_k': bond_angle_force_constant,'bb_bb_sc_angle_k': bond_angle_force_constant,'bb_sc_sc_angle_k': bond_angle_force_constant,'sc_sc_sc_angle_k': bond_angle_force_constant, 'sc_bb_sc_angle_k': bond_angle_force_constant, 'sc_sc_bb_angle_k': bond_angle_force_constant}
+equil_bond_angle = 92
+equil_bond_angles = {'bb_bb_bb_angle_0': equil_bond_angle,'bb_bb_sc_angle_0': equil_bond_angle,'bb_sc_sc_angle_0': equil_bond_angle,'sc_sc_sc_angle_0': equil_bond_angle, 'sc_bb_sc_angle_0': equil_bond_angle,'sc_sc_bb_angle_0': equil_bond_angle}
+
+# Torsion properties
+torsion_force_constant = 2000
+torsion_force_constants = {'bb_bb_bb_bb_torsion_k': torsion_force_constant,'bb_bb_bb_sc_torsion_k': torsion_force_constant,'bb_bb_sc_sc_torsion_k': torsion_force_constant, 'bb_sc_sc_sc_torsion_k': torsion_force_constant, 'sc_bb_bb_sc_torsion_k': torsion_force_constant, 'bb_sc_sc_bb_torsion_k': torsion_force_constant, 'sc_sc_sc_sc_torsion_k': torsion_force_constant,  'sc_bb_bb_bb_torsion_k': torsion_force_constant}
+equil_torsion_angle = 52
+equil_torsion_angles = {'bb_bb_bb_bb_torsion_0': equil_torsion_angle,'bb_bb_bb_sc_torsion_0': equil_torsion_angle,'bb_bb_sc_sc_torsion_0': equil_torsion_angle, 'bb_sc_sc_sc_torsion_0': equil_torsion_angle, 'sc_bb_bb_sc_torsion_0': equil_torsion_angle, 'bb_sc_sc_bb_torsion_0': equil_torsion_angle, 'sc_sc_sc_sc_torsion_0': equil_torsion_angle, 'sc_bb_bb_bb_torsion_0': equil_torsion_angle}
+
+sigma = 2.5 * bond_length
+#sigma_list = [ (1.5 + i*0.1) * bond_length for i in range(grid_size)]
+epsilon = 0.05 * unit.kilocalorie_per_mole
+sigmas = {'bb_bb_sigma': sigma,'bb_sc_sigma': sigma,'sc_sc_sigma': sigma}
+epsilons = {'bb_bb_eps': epsilon,'bb_sc_eps': epsilon,'sc_sc_eps': epsilon}
+cgmodel = CGModel(polymer_length=polymer_length,backbone_lengths=backbone_lengths,sidechain_lengths=sidechain_lengths,sidechain_positions=sidechain_positions,masses=masses,sigmas=sigmas,epsilons=epsilons,bond_lengths=bond_lengths,bond_force_constants=bond_force_constants,bond_angle_force_constants=bond_angle_force_constants,torsion_force_constants=torsion_force_constants,equil_bond_angles=equil_bond_angles,equil_torsion_angles=equil_torsion_angles,include_nonbonded_forces=include_nonbonded_forces,include_bond_forces=include_bond_forces,include_bond_angle_forces=include_bond_angle_forces,include_torsion_forces=include_torsion_forces,constrain_bonds=constrain_bonds)
+
+ensemble = get_ensemble(cgmodel,low_energy=True)
+energies = []
+index = 1
+for pose in ensemble:
+    cgmodel.positions = pose
+    cgmodel.simulation = build_mm_simulation(cgmodel.topology,cgmodel.system,cgmodel.positions)
+    energy = cgmodel.simulation.context.getState(getEnergy=True).getPotentialEnergy()
+    write_pdbfile_without_topology(cgmodel,str("pose_"+str(index)+".pdb"),energy=energy)   
+    energies.append(energy._value)
+    index = index + 1
+energies = np.array([energy for energy in energies])
+lowest_energy_pose_index = np.argmin(energies)
+print(lowest_energy_pose_index)
+exit()
