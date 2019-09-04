0f41adbad154fc48eb77729bdda1ea7ff775f421
garrettameek <garrett.a.meek@gmail.com>
2019-09-04 16:43:08 -0600

    Updated source code structure.
    Former-commit-id: 215ed357745bac1ab43f90d463a29753af7b9813
    Former-commit-id: c2651205f8ea2e296d027377d868729251d978f4
    Former-commit-id: 4d4be196e8643b8dba8ee986bd5d381639184d8a [formerly 71853ba2082858c02c9627d48f8d0e68f75dc3ba]
    Former-commit-id: a7b6d408e0bb1bbe11337956704d023744825caf
    Former-commit-id: 690b5e1d07ba099fe6e6a9acc46f580316e9c166 [formerly e01edb05f64612bb159c466e56649c8de71492d5]
    Former-commit-id: d5b1acbe3c4ee7bf20ef0d8c4ca10c043e32de31

---
+++ util.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as pyplot
+# OpenMM utilities
+import mdtraj as md
+from simtk import unit
+# foldamers utilities
+from foldamers.src.cg_model.cgmodel import basic_cgmodel
+from foldamers.src.ensembles.ens_build import 
+
+def reweight(cgmodel,sigma_steps=3,epsilon_steps=3):
+        return(cgmodel)
