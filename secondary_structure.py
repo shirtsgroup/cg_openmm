9775d2f5ed351a9235380be2317088d21114d38d
garrettameek <garrett.a.meek@gmail.com>
2019-09-13 17:37:02 -0600

    Updated documentation, added functions for native contact evaluation, and updated code for evaluation of helical parameters.
    Former-commit-id: b321c55cf216a4ad79c20f3035567fa8d1511734
    Former-commit-id: 95d88aa34ea4f46eab04c3d5887e4015fe755e60
    Former-commit-id: c596b685458ca0fb2420afc0b8283340748cee5f [formerly 48f51d5f2ea4612b3e74c7d903c2fcda54b92c9a]
    Former-commit-id: 0de69bd1ce7941f8a0ec28dabfe1d2d638de21f2
    Former-commit-id: 01e81e5ee1d6c205377aa96f4c1f3e630a7c7758 [formerly 789ada33986e9229550c21d85c93b8718314cf0c]
    Former-commit-id: 0db8b8b0f7204f7276ba74cafb574ae62641f080

--- secondary_structure.py
+++ secondary_structure.py
@@ -10,13 +10,29 @@ from foldamers.utilities.iotools import write_pdbfile_without_topology
 
 def fraction_native_contacts(cgmodel,positions,native_structure,cutoff_distance=None):
         """
-        
+        Given a coarse grained model, positions for that model, and positions for the native structure, this function calculates the fraction of native contacts for the model.
+
+        :param cgmodel: CGModel() class object
+        :type cgmodel: class
+
+        :param positions: Positions for the particles in a coarse grained model.
+        :type positions: np.array( float * unit.angstrom ( num_particles x 3 ) )
+
+        :param native_structure: Positions for the native structure.
+        :type native_structure: np.array( float * unit.angstrom ( num_particles x 3 ) )
+
+        :param cutoff_distance: The maximum distance for two nonbonded particles that are defined as "native",default=None
+        :type cutoff_distance: `Quantity() <https://docs.openmm.org/development/api-python/generated/simtk.unit.quantity.Quantity.html>`_
+
+        :returns:
+          - Q ( float ) - The fraction of native contacts for the model with the current structure.
 
         """
-        cutoff_distance = 1.1 * cgmodel.get_sigma(0)
+        if cutoff_distance == None:
+          cutoff_distance = 1.1 * cgmodel.get_sigma(0)
 
         nonbonded_interaction_list = cgmodel.nonbonded_interaction_list
-        #print("There are "+str(len(nonbonded_interaction_list))+" total nonbonded interaction possibilities.")
+        print("There are "+str(len(nonbonded_interaction_list))+" total nonbonded interaction possibilities.")
         native_structure_distances = distances(nonbonded_interaction_list,native_structure)
         current_structure_distances = distances(nonbonded_interaction_list,positions)
         native_distances = []
@@ -27,22 +43,38 @@ def fraction_native_contacts(cgmodel,positions,native_structure,cutoff_distance=
             native_distances.append(native_structure_distances[interaction])
             native_interaction_list.append(interaction)
         total_native_interactions = len(native_interaction_list)
+        print("There are "+str(total_native_interactions)+" native interactions.")
 
         current_distances = []
         current_structure_interaction_list = []
         for interaction in range(len(nonbonded_interaction_list)):
           if interaction in native_interaction_list:
-            if current_structure_distances[interaction].__lt__(cutoff_distance):
+            if current_structure_distances[interaction].__lt__(1.1*native_structure_distances[interaction]):
               current_distances.append(current_structure_distances[interaction])
               current_structure_interaction_list.append(interaction)
-        current_structure_native_interactions = len(current_structure_interaction_list) 
+        current_structure_native_interactions = len(current_structure_interaction_list)
+        print("The current structure has "+str(current_structure_native_interactions)+" native interactions")
         Q = current_structure_native_interactions / total_native_interactions
         return(Q)
 
 def get_helical_parameters(cgmodel):
         """
+        Given a coarse grained model as input, this function uses the `kHelios software package <https://pubs.acs.org/doi/10.1021/acs.jcim.6b00721>`_ to analyze the helical properties of the model.
+
+        :param cgmodel: CGModel() class object
+        :type cgmodel: class
+
+        :returns:
+          - pitch ( float ) - The distance between monomers in adjacent turns of a helix
+          - radius ( float ) - The radius of the helix
+          - monomers_per_turn ( float ) - The number of monomrs per turn of the helix
+          - residual ( float ) - The average distance of all backbone particles from a circle projected onto the x-y plane.  Used to determine the accuracy of the helical axis, as fit to the input data.  Units are in Angstroms.
+
+        .. warning:: This function requires a pre-installed version of `kHelios <https://pubs.acs.org/doi/10.1021/acs.jcim.6b00721>`_ .  Because kHelios is formatted to accept input job scripts, this function writes and executes a job script for kHelios.  In order to function properly, the user must redefine the 'helios_path' variable for their system.
+
         """
-        helios_path = str(str(os.path.realpath(__file__).split('/secondary_structure')[0])+str("/helios.o"))
+        helios_path = str('/home/gmeek/Foldamers/foldamers/foldamers/parameters/helios.o')
+        cgmodel = orient_along_z_axis(cgmodel)
         write_pdbfile_without_topology(cgmodel,"temp_pitch.pdb")
         kHelix_run_file = "run_kHelix.sh"
         file = open(kHelix_run_file,"w")
@@ -52,19 +84,20 @@ def get_helical_parameters(cgmodel):
         file.write('inputhelix $1\n')
         file.write('helixout_name kHelix.out\n')
         file.write('coord_type 1\n')
-        file.write('num_grid 360\n')
+        file.write('num_grid 20\n')
         file.write('natoms '+str(round(cgmodel.num_beads/2))+'\n')
         file.write('nframes 1\n')
         file.write('grid_phi_beg 0\n')
-        file.write('grid_phi_end 180\n')
+        file.write('grid_phi_end 20\n')
         file.write('grid_theta_beg 0\n')
-        file.write('grid_theta_end 180\n')
+        file.write('grid_theta_end 20\n')
         file.write('helix_atom_names X1\n')
         file.write('print_to_plot 1\n')
         file.write('EOF\n')
         file.write(str(helios_path)+' input\n')
         #file.write('done\n')        
         file.close()
+        subprocess.run(['chmod','+x','run_kHelix.sh'])
         subprocess.run([str(str(os.getcwd())+"/"+str(kHelix_run_file)),"temp_pitch.pdb",">","helios_output"])
         #os.remove("helios_output")
         file = open("kHelix.out",mode="r")
@@ -72,6 +105,7 @@ def get_helical_parameters(cgmodel):
         line_index = 1
         for line in output:
           if line_index == 43:
+            residual = line.split()[2]
             radius = line.split()[3]
             pitch = line.split()[4]
             sweep = float(line.split()[5])
@@ -79,14 +113,22 @@ def get_helical_parameters(cgmodel):
             break
           line_index = line_index + 1
           
-        return(pitch,radius,monomers_per_turn)
+        return(pitch,radius,monomers_per_turn,residual)
 
-def get_helical_data(cgmodel):
+def orient_along_z_axis(cgmodel,plot_projections=False):
         """
+        Given a coarse grained model as input, this function orients the model along the z-axis.
+
+        :param cgmodel: CGModel() class object
+        :type cgmodel: class
+
+        :param plot_projections: Variable indicating whether or not to plot intermediate projections/operations during identification of a helical axis.
+
+        :returns:
+           - cgmodel ( class ) - CGModel() class object, with positions oriented so that the helical axis is along the z-axis
+
         """
         plot_projections = False
-        #file = open("before_rotation.pdb","w")
-        #PDBFile.writeFile(cgmodel.topology,cgmodel.positions,file=file)
         positions = np.array([[float(i.in_units_of(unit.angstrom)._value) for i in position] for position in cgmodel.positions])
         # 1) Get the backbone particle positions
         backbone_positions = []
@@ -160,16 +202,26 @@ def get_helical_data(cgmodel):
         # Calculate angle from y-axis:
         y_axis_angle = np.arctan(slope)
         y_axis_rotation_matrix = spatial.transform.Rotation.from_euler('xyz', [0.,y_axis_angle,0.], degrees=False)
-        final_positions = y_axis_rotation_matrix.apply(x_oriented_positions)
 
-        print(final_positions)
+        new_positions = y_axis_rotation_matrix.apply(x_oriented_positions)
+
+        # 8) For comparison with kHelios output, rotate the molecule again so that the helical (x) axis is oriented along the z axis instead.
+
+        z_axis_angle = 3.1415/2.0
+        z_axis_rotation_matrix = spatial.transform.Rotation.from_euler('xyz',[0.,z_axis_angle,0.0], degrees=False)
+        final_positions = z_axis_rotation_matrix.apply(new_positions)
 
         cgmodel.positions = unit.Quantity(final_positions,unit.angstrom)
 
         file = open("after_rotation.pdb","w")
         PDBFile.writeFile(cgmodel.topology,cgmodel.positions,file=file)
 
-        # 8) Using these transformed coordinates, calculate the helical parameters for this structure:
+        return(cgmodel)
+
+def get_helical_data(cgmodel):
+        """
+        """
+        cgmodel = orient_along_z_axis(cgmodel) 
 
         # Get the new backbone particle positions
         backbone_positions = []
@@ -192,25 +244,21 @@ def get_helical_data(cgmodel):
             rotation = np.arctan(position[1]/position[2])
             last_angle = rotation
             rotations = rotations + rotation
-
-        print(rotations)
-        print(axis_distances)
-        exit()
             
         radius = mean(np.array([float(dist.in_units_of(unit.angstrom)._value) for dist in axis_distances]))
         particles_per_turn = float(cgmodel.polymer_length/(rotations/6.28))
 
         # pitch
         #
-        # Shift all coordinates so that the first backbone atom has x=0
+        # Shift all coordinates so that the first backbone atom has z=0
 
-        shift = - final_positions[0][0]
+        shift = - cgmodel.positions[0][2]._value
 
         axis_deltas = []
-        for position in final_positions:
-          position[0] = position[0] + shift
-          if abs(position[0] - final_positions[0][0]) > 0:
-            axis_deltas.append(float(position[0]-final_positions[0][0]))
+        for position in cgmodel.positions:
+          position[0]._value = position[0]._value + shift
+          if abs(position[0]._value - cgmodel.positions[0][0]._value) > 0:
+            axis_deltas.append(float(position[0]._value-cgmodel.positions[0][0]._value))
         average_delta = mean(axis_deltas)
         pitch = average_delta * particles_per_turn
 
