6bf7851338fbf0c198c18feb86a1ecfa03cce773
garrettameek <garrett.a.meek@gmail.com>
2019-09-19 19:19:49 -0600

    Minor changes to the model builder.
    Former-commit-id: 07c4aa8570444f685f4c862c0ab410a54e92428c
    Former-commit-id: 9925aa0d8cd885fec6ab254599812de5a99b7ce8
    Former-commit-id: a95ad365705382021189413d79157cd6c3185b63 [formerly 5fc1ede6d8b460b5a9bf5250f9b8590198418d79]
    Former-commit-id: 84602ae2df0884d79f4efe1033b90b47fb386055
    Former-commit-id: ca4c8cf9360782e4fbd7d986d3a96aaa576eb853 [formerly ecabd4ff4d86d5fc13e520aa0f0e61d2e995e03e]
    Former-commit-id: 971648344416ad8098cb63c45e4264715db31ab6

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ import numpy as np
 import math, random, statistics
 from simtk import unit
 from simtk.openmm.app.pdbfile import PDBFile
-from cg_openmm.build.cg_build import *
+from cg_openmm.build.cg_build import build_system, build_topology
 from cg_openmm.simulation.tools import *
 from foldamers.cg_model.cgmodel import *
 from foldamers.utilities.iotools import *
