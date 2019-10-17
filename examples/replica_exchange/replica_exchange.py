7b64e0e04e63f53fe295a57465924c1de254429c
garrettameek <garrett.a.meek@gmail.com>
2019-10-17 11:12:02 -0600

    Updated the NVT and replica exchange simulation examples.

--- examples/replica_exchange/replica_exchange.py
+++ examples/replica_exchange/replica_exchange.py
@@ -4,6 +4,13 @@ from foldamers.cg_model.cgmodel import CGModel
 from foldamers.parameters.reweight import get_temperature_list
 from cg_openmm.simulation.rep_exch import *
 
+###
+#
+# This example demonstrates how to run a Yank replica exchange simulation
+# using a "CGModel" object built with the 'foldamers' software package.
+#
+###
+
 # Job settings
 output_directory = 'output'
 if not os.path.exists(output_directory):
@@ -35,35 +42,41 @@ include_nonbonded_forces=True
 include_torsion_forces=True
 constrain_bonds = True
 
-# Particle properties
-mass = 100.0 * unit.amu
-masses = {'backbone_bead_masses': mass, 'sidechain_bead_masses': mass}
+# Bond definitions
 bond_length = 7.5 * unit.angstrom
 bond_lengths = {'bb_bb_bond_length': bond_length,'bb_sc_bond_length': bond_length,'sc_sc_bond_length': bond_length}
-bond_force_constant = 1250 * unit.kilojoule_per_mole / unit.nanometer / unit.nanometer
+bond_force_constant = 0 * unit.kilocalorie_per_mole / unit.nanometer / unit.nanometer
 bond_force_constants = {'bb_bb_bond_k': bond_force_constant, 'bb_sc_bond_k': bond_force_constant, 'sc_sc_bond_k': bond_force_constant}
-sigma = 2.5 * bond_length
-sigmas = {'bb_bb_sigma': sigma,'bb_sc_sigma': sigma,'sc_sc_sigma': sigma}
+
+# Particle definitions
+mass = 100.0 * unit.amu
+masses = {'backbone_bead_masses': mass, 'sidechain_bead_masses': mass}
+r_min =  3.0 * bond_length # Lennard-Jones potential r_min
+sigma =  r_min / (2.0**(1/6)) # Factor of /(2.0**(1/6)) is applied to convert r_min to sigma
+sigmas = {'bb_sigma': sigma,'sc_sigma': sigma}
 epsilon = 0.5 * unit.kilocalorie_per_mole
-epsilons = {'bb_bb_eps': epsilon,'bb_sc_eps': epsilon,'sc_sc_eps': 0.5 * epsilon}
+epsilons = {'bb_eps': epsilon,'sc_eps': epsilon}
 
-# Bond angle properties
-bond_angle_force_constant = 200 * unit.kilojoule_per_mole / unit.radian / unit.radian
-bond_angle_force_constants = {'bb_bb_bb_angle_k': bond_angle_force_constant,'bb_bb_sc_angle_k': bond_angle_force_constant,'bb_sc_sc_angle_k': bond_angle_force_constant,'sc_sc_sc_angle_k': bond_angle_force_constant, 'sc_bb_sc_angle_k': bond_angle_force_constant, 'sc_sc_bb_angle_k': bond_angle_force_constant}
-equil_bond_angle = 92
-equil_bond_angles = {'bb_bb_bb_angle_0': equil_bond_angle,'bb_bb_sc_angle_0': equil_bond_angle,'bb_sc_sc_angle_0': equil_bond_angle,'sc_sc_sc_angle_0': equil_bond_angle, 'sc_bb_sc_angle_0': equil_bond_angle,'sc_sc_bb_angle_0': equil_bond_angle}
+# Bond angle definitions
+bond_angle_force_constant = 0.5 * unit.kilocalorie_per_mole / unit.radian / unit.radian
+bond_angle_force_constants = {'bb_bb_bb_angle_k': bond_angle_force_constant,'bb_bb_sc_angle_k': bond_angle_force_constant}
+bb_bb_bb_equil_bond_angle = 120.0 * (3.14/180.0) # OpenMM requires angle definitions in units of radians
+bb_bb_sc_equil_bond_angle = 120.0 * (3.14/180.0)
+equil_bond_angles = {'bb_bb_bb_angle_0': bb_bb_bb_equil_bond_angle,'bb_bb_sc_angle_0': bb_bb_sc_equil_bond_angle}
 
-# Torsion properties
-torsion_force_constant = 200
-torsion_force_constants = {'bb_bb_bb_bb_torsion_k': torsion_force_constant,'bb_bb_bb_sc_torsion_k': torsion_force_constant,'bb_bb_sc_sc_torsion_k': torsion_force_constant, 'bb_sc_sc_sc_torsion_k': torsion_force_constant, 'sc_bb_bb_sc_torsion_k': torsion_force_constant, 'bb_sc_sc_bb_torsion_k': torsion_force_constant, 'sc_sc_sc_sc_torsion_k': torsion_force_constant,  'sc_bb_bb_bb_torsion_k': torsion_force_constant}
-equil_torsion_angle = 52
-equil_torsion_angles = {'bb_bb_bb_bb_torsion_0': equil_torsion_angle,'bb_bb_bb_sc_torsion_0': equil_torsion_angle,'bb_bb_sc_sc_torsion_0': equil_torsion_angle, 'bb_sc_sc_sc_torsion_0': equil_torsion_angle, 'sc_bb_bb_sc_torsion_0': equil_torsion_angle, 'bb_sc_sc_bb_torsion_0': equil_torsion_angle, 'sc_sc_sc_sc_torsion_0': equil_torsion_angle, 'sc_bb_bb_bb_torsion_0': equil_torsion_angle}
+# Torsion angle definitions
+torsion_force_constant = 0.5 * unit.kilocalorie_per_mole / unit.radian / unit.radian
+torsion_force_constants = {'bb_bb_bb_bb_torsion_k': torsion_force_constant}
+bb_bb_bb_bb_equil_torsion_angle = 78.0 * (3.14/180.0) # OpenMM requires angle definitions in units of radians
+bb_bb_bb_sc_equil_torsion_angle = 78.0 * (3.14/180.0)
+equil_torsion_angles = {'bb_bb_bb_bb_torsion_0': bb_bb_bb_bb_equil_torsion_angle}
+torsion_periodicities = {'bb_bb_bb_bb_period': 1}
 
-# Get positions from local file
+# Get initial positions from local file
 positions = PDBFile("helix.pdb").getPositions()
 
 # Build a coarse grained model
-cgmodel = CGModel(polymer_length=polymer_length,backbone_lengths=backbone_lengths,sidechain_lengths=sidechain_lengths,sidechain_positions=sidechain_positions,masses=masses,sigmas=sigmas,epsilons=epsilons,bond_lengths=bond_lengths,bond_force_constants=bond_force_constants,bond_angle_force_constants=bond_angle_force_constants,torsion_force_constants=torsion_force_constants,equil_bond_angles=equil_bond_angles,equil_torsion_angles=equil_torsion_angles,include_nonbonded_forces=include_nonbonded_forces,include_bond_forces=include_bond_forces,include_bond_angle_forces=include_bond_angle_forces,include_torsion_forces=include_torsion_forces,constrain_bonds=constrain_bonds,positions=positions)
+cgmodel = CGModel(polymer_length=polymer_length,backbone_lengths=backbone_lengths,sidechain_lengths=sidechain_lengths,sidechain_positions=sidechain_positions,masses=masses,sigmas=sigmas,epsilons=epsilons,bond_lengths=bond_lengths,bond_force_constants=bond_force_constants,bond_angle_force_constants=bond_angle_force_constants,torsion_force_constants=torsion_force_constants,equil_bond_angles=equil_bond_angles,equil_torsion_angles=equil_torsion_angles,torsion_periodicities=torsion_periodicities,include_nonbonded_forces=include_nonbonded_forces,include_bond_forces=include_bond_forces,include_bond_angle_forces=include_bond_angle_forces,include_torsion_forces=include_torsion_forces,constrain_bonds=constrain_bonds,positions=positions)
 
 # Run replica exchange simulations *if* there is not existing data in 'output_directory'.
 # If there is data in 'output_directory', read that data instead.
