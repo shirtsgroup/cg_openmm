421ae89476fca254cffebd292145d39fb8c28403
garrettameek <garrett.a.meek@gmail.com>
2019-10-21 13:03:18 -0600

    Another attempt to fix requirements.txt for RTD
    Former-commit-id: 30829aafe4c96cefa6d91462eebcbd6ac8a2f0e3 [formerly 4f4fddcc8a7f9e947fee293255b22a494f6ca77b]
    Former-commit-id: d44cf5344a901de14138939b8c3c2797b5df5276
    Former-commit-id: eb1f12da49eb6cd6ff30a211e1bd2c3ddeaa111a [formerly df74b3ae2c44b7cf7c810ccfca8b080e37fd8a53]
    Former-commit-id: 584941c651e9dc5391ef2be4460288f1600feb95
    Former-commit-id: 8482d55b3f5a55508d6e007d17fbb8532d50ce2e [formerly ee9a9ee171b130f46c9e0e6e74a9edede5fbd44a]
    Former-commit-id: 02faa847d171db06d9c7f70c1b0fd0dc2acdc271

--- cgmodel.py
+++ cgmodel.py
@@ -5,7 +5,7 @@ from simtk import openmm as mm
 from simtk.openmm.app.pdbfile import PDBFile
 from simtk.openmm.app.topology import Topology
 from simtk.openmm.app.topology import Residue
-import simtk.openmm.app.element as elem
+from simtk.openmm.app import element as elem
 import foldamers
 from foldamers.utilities.util import random_positions
 from foldamers.utilities import util
