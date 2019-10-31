7faf984b5eb1920ba045a1eb39286e4c62b5c43d
garrettameek <garrett.a.meek@gmail.com>
2019-10-31 12:15:19 -0600

    Updated the scan_torsions example to enable analysis of more thermodynamic properties.
    Former-commit-id: 00f02dcd801d14e69847169c9b4144a2d9f828e4 [formerly ba107509f97d841cb1bac1dd11c20c31ab44c383]
    Former-commit-id: 8301b7082c67a0679146d10090711c159ce21f90
    Former-commit-id: fed19dc277371b8fa4e55d88db805f46e561027c [formerly 448cf8b2913d744550d398487438761599b073f8]
    Former-commit-id: c032e6ef4199dd4b2cf8dc6532adfd3501fc2387
    Former-commit-id: 9fb0d429b73c90efff53d887b231fcbe9dc8d99a [formerly ba18945c07fdb6ece9884734a807c6ecb7b30830]
    Former-commit-id: 76bd76a7e143ee83e0e20f332f3ce0910cb4fdbc

--- varying_model_parameters/scan_torsions/scan_torsions.py
+++ varying_model_parameters/scan_torsions/scan_torsions.py
@@ -6,12 +6,16 @@ from simtk import unit
 from simtk.openmm.app.pdbfile import PDBFile
 from foldamers.cg_model.cgmodel import CGModel
 from foldamers.parameters.reweight import *
+from foldamers.thermo.calc import *
 from foldamers.ensembles.ens_build import *
 from cg_openmm.simulation.rep_exch import *
 from cg_openmm.simulation.tools import *
 
 # Job settings
 scan_sc_bb_bb_sc_torsions = True
+calculate_dQ = True
+calculate_free_energies = True
+evaluate_heat_capacity = True
 output_directory = 'output'
 if not os.path.exists(output_directory):
   os.mkdir(output_directory)
@@ -67,7 +71,7 @@ if scan_sc_bb_bb_sc_torsions == True:
   bb_bb_bb_bb_equil_torsion_angle = 78.0 * (3.14/180.0) # OpenMM defaults to units of radians for angle definitions
   sc_bb_bb_sc_equil_torsion_angle = 120.0 * (3.14/180.0)
   equil_torsion_angles = {'bb_bb_bb_bb_torsion_0': bb_bb_bb_bb_equil_torsion_angle,'sc_bb_bb_sc_torsion_0': sc_bb_bb_sc_equil_torsion_angle}
-  torsion_periodicities = {'bb_bb_bb_bb_period': 1,'sc_bb_bb_sc_period': 2}
+  torsion_periodicities = {'bb_bb_bb_bb_period': 1,'sc_bb_bb_sc_period': 1}
 else:
   torsion_force_constants = {'bb_bb_bb_bb_torsion_k': torsion_force_constant}
   bb_bb_bb_bb_equil_torsion_angle = 78.0 * (3.14/180.0) # OpenMM defaults to units of radians for angle definitions
@@ -108,16 +112,25 @@ if scan_sc_bb_bb_sc_torsions == True:
 else:
   sc_bb_bb_sc_equil_torsion_angles = [0.0]
 
-# Set parameters for evaluating native contacts
-native_structure_contact_distance_cutoff = 1.15 * cgmodel.get_sigma(0) # This distance cutoff determines which nonbonded interactions are considered 'native' contacts
-native_fraction_cutoff = 0.95 # The threshold fraction of native contacts above which a pose is considered 'native'
-nonnative_fraction_cutoff = 0.95 # The threshold fraction of native contacts below which a pose is considered 'nonnative'
-native_ensemble_size = 10
-nonnative_ensemble_size = 10
-decorrelate=True
+if calculate_dQ:
+ # Set parameters for evaluating native contacts
+ native_structure_contact_distance_cutoff = 1.00 * cgmodel.get_sigma(0) # This distance cutoff determines which nonbonded interactions are considered 'native' contacts
+ native_fraction_cutoff = 0.95 # The threshold fraction of native contacts above which a pose is considered 'native'
+ nonnative_fraction_cutoff = 0.95 # The threshold fraction of native contacts below which a pose is considered 'nonnative'
+ native_ensemble_size = 10
+ nonnative_ensemble_size = 10
+ decorrelate=True
 
-# Build an array to store data we will read from the output
+# Build arrays to store data for each model parameter scan/grid point
 dQ_list = []
+df_ij_list = []
+ddf_ij_list = []
+Delta_u_list = []
+dDelta_u_list = []
+Delta_s_list = []
+dDelta_s_list = []
+C_v_list = []
+dC_v_list = []
 
 # This is where we start evaluating the properties of models with different equilibrium torsion angles
 for sc_bb_bb_sc_equil_torsion_angle in sc_bb_bb_sc_equil_torsion_angles:
@@ -161,14 +174,15 @@ for sc_bb_bb_sc_equil_torsion_angle in sc_bb_bb_sc_equil_torsion_angles:
     PDBFile.writeFile(cgmodel.topology,native_structure,file=file)
     file.close()
 
-  native_structure_contact_distance_cutoff = 1.15 * cgmodel.get_sigma(0) # This distance cutoff determines which nonbonded interactions are considered 'native' contacts
-  native_fraction_cutoff = 0.95 # The threshold fraction of native contacts above which a pose is considered 'native'
-  nonnative_fraction_cutoff = 0.95 # The threshold fraction of native contacts below which a pose is considered 'nonnative'
-  native_ensemble_size = 10
-  nonnative_ensemble_size = 100
-  decorrelate=True
-  native_ensemble,native_ensemble_energies,nonnative_ensemble,nonnative_ensemble_energies = get_ensembles_from_replica_positions(cgmodel,replica_positions,replica_energies,temperature_list,decorrelate=decorrelate,native_fraction_cutoff=native_fraction_cutoff,nonnative_fraction_cutoff=nonnative_fraction_cutoff,native_structure_contact_distance_cutoff=native_structure_contact_distance_cutoff,native_ensemble_size=native_ensemble_size,nonnative_ensemble_size=nonnative_ensemble_size)
-  if len(native_ensemble_energies) != native_ensemble_size or len(nonnative_ensemble_energies) != nonnative_ensemble_size:
+  if calculate_dQ:
+   native_structure_contact_distance_cutoff = 1.15 * cgmodel.get_sigma(0) # This distance cutoff determines which nonbonded interactions are considered 'native' contacts
+   native_fraction_cutoff = 0.95 # The threshold fraction of native contacts above which a pose is considered 'native'
+   nonnative_fraction_cutoff = 0.95 # The threshold fraction of native contacts below which a pose is considered 'nonnative'
+   native_ensemble_size = 10
+   nonnative_ensemble_size = 100
+   decorrelate=True
+   native_ensemble,native_ensemble_energies,nonnative_ensemble,nonnative_ensemble_energies = get_ensembles_from_replica_positions(cgmodel,replica_positions,replica_energies,temperature_list,decorrelate=decorrelate,native_fraction_cutoff=native_fraction_cutoff,nonnative_fraction_cutoff=nonnative_fraction_cutoff,native_structure_contact_distance_cutoff=native_structure_contact_distance_cutoff,native_ensemble_size=native_ensemble_size,nonnative_ensemble_size=nonnative_ensemble_size)
+   if len(native_ensemble_energies) != native_ensemble_size or len(nonnative_ensemble_energies) != nonnative_ensemble_size:
     print("ERROR: attempt to generate native and nonnative ensembles was unsuccessful.")
     print(str(len(native_ensemble_energies))+" native ensemble members were generated ("+str(native_ensemble_size)+" were requested),")
     print("and "+str(len(nonnative_ensemble_energies))+" non-native ensemble members were generated ("+str(nonnative_ensemble_size)+" were requested).")
@@ -176,54 +190,71 @@ for sc_bb_bb_sc_equil_torsion_angle in sc_bb_bb_sc_equil_torsion_angles:
     print("and the 'nonnative_fraction_cutoff' parameter (current value="+str(nonnative_fraction_cutoff)+")")
     print("to see if either of these approaches fixes the problem.")
     exit()
-  if scan_sc_bb_bb_sc_torsions == True:
+   if scan_sc_bb_bb_sc_torsions == True:
     nonnative_ensemble_directory = str(str(output_directory)+"/ens_"+str(round(bb_bb_bb_bb_equil_torsion_angle*(180.0/3.14),1))+"_"+str(round(sc_bb_bb_sc_equil_torsion_angle*(180.0/3.14),1))+"_nonnative")
     native_ensemble_directory = str(str(output_directory)+"/ens_"+str(round(bb_bb_bb_bb_equil_torsion_angle*(180.0/3.14),1))+"_"+str(round(sc_bb_bb_sc_equil_torsion_angle*(180.0/3.14),1))+"_native")
-  else:
+   else:
     nonnative_ensemble_directory = str(str(output_directory)+"/ens_"+str(round(bb_bb_bb_bb_equil_torsion_angle*(180.0/3.14),1))+"_nonnative")
     native_ensemble_directory = str(str(output_directory)+"/ens_"+str(round(bb_bb_bb_bb_equil_torsion_angle*(180.0/3.14),1))+"_native")
-  # We build an ensemble of nonnative poses for energetic comparison with the native pose.
-  if os.path.exists(nonnative_ensemble_directory):
+   # We build an ensemble of nonnative poses for energetic comparison with the native pose.
+   if os.path.exists(nonnative_ensemble_directory):
     nonnative_ensemble,nonnative_ensemble_energies = get_ensemble_data(cgmodel,nonnative_ensemble_directory)
     if len(nonnative_ensemble) != nonnative_ensemble_size:
       print("ERROR: "+str(len(nonnative_ensemble_energies))+" nonnative poses were found in existing output folders, but "+str(nonnative_ensemble_size)+" poses were requested.")
       print("This probably means that the requested ensemble size changed since the script was last run.")
       exit()
-  else:
+   else:
     os.mkdir(nonnative_ensemble_directory)
     for pose in nonnative_ensemble:
       cgmodel.positions = pose
       write_ensemble_pdb(cgmodel,ensemble_directory=nonnative_ensemble_directory)
 
-  nonnative_ensemble_Q = []
-  for pose in nonnative_ensemble:
+   nonnative_ensemble_Q = []
+   for pose in nonnative_ensemble:
     Q = fraction_native_contacts(cgmodel,pose,native_structure)
     nonnative_ensemble_Q.append(Q)
-  nonnative_ensemble_Q = np.array([Q for Q in nonnative_ensemble_Q])
-  mean_nonnative_contacts = mean(nonnative_ensemble_Q)
-  print("The mean fraction of native contacts for this model is: "+str(mean_nonnative_contacts))
+   nonnative_ensemble_Q = np.array([Q for Q in nonnative_ensemble_Q])
+   mean_nonnative_contacts = mean(nonnative_ensemble_Q)
+   print("The mean fraction of native contacts for this model is: "+str(mean_nonnative_contacts))
 
-  # We build an ensemble of native poses in order to understand the energy distribution around the folded state.
-  if os.path.exists(native_ensemble_directory):
+   # We build an ensemble of native poses in order to understand the energy distribution around the folded state.
+   if os.path.exists(native_ensemble_directory):
     native_ensemble,native_ensemble_energies = get_ensemble_data(cgmodel,native_ensemble_directory)
     if len(native_ensemble_energies) != native_ensemble_size:
       print("ERROR: "+str(len(native_ensemble_energies))+" native poses were found in existing output folders, but "+str(native_ensemble_size)+" poses were requested.")
       print("This probably means that the requested ensemble size changed since the script was last run.")
       exit()
-  else:
+   else:
     os.mkdir(native_ensemble_directory)
     for pose in native_ensemble:
       cgmodel.positions = pose
       write_ensemble_pdb(cgmodel,ensemble_directory=native_ensemble_directory)
 
-  # Get the average change in the fraction of native contacts during folding (dQ),
-  # calculated as the difference between the average fraction of native contacts
-  # in the nonnative ensemble.
-  # A large dQ means the model/structure has a stable folded state.
-  # A small dQ means the model/structure does not have a stable folded state.
-  dQ = 1.0 - mean_nonnative_contacts
-  dQ_list.append(dQ)
+   # Get the average change in the fraction of native contacts during folding (dQ),
+   # calculated as the difference between the average fraction of native contacts
+   # in the nonnative ensemble.
+   # A large dQ means the model/structure has a stable folded state.
+   # A small dQ means the model/structure does not have a stable folded state.
+   dQ = 1.0 - mean_nonnative_contacts
+   dQ_list.append(dQ)
 
+  if calculate_free_energies:
+   num_intermediate_states = 1
+   mbar,E_kn,E_expect,dE_expect,new_temp_list = get_mbar_expectation(replica_energies,temperature_list,num_intermediate_states)
+   df_ij,ddf_ij = get_free_energy_differences(mbar)
+   df_ij_list.append(df_ij)
+   ddf_ij_list.append(ddf_ij)
+   Delta_s,dDelta_s = get_entropy_differences(mbar)
+   Delta_s_list.append(Delta_s)
+   dDelta_s_list.append(dDelta_s)
+   Delta_u,dDelta_u = get_enthalpy_differences(mbar)
+   Delta_u_list.append(Delta_u)
+   dDelta_u_list.append(dDelta_u)
+
+  if evaluate_heat_capacity:
+   C_v,dC_v,new_temperature_list = get_heat_capacity(replica_energies,temperature_list,num_intermediate_states=1)
+   C_v_list.append(C_v)
+   dC_v_list.append(dC_v)
 
 if scan_sc_bb_bb_sc_torsions == True:
   file_name = "dQ_for_variable_equil_torsion_angles.png"
@@ -245,20 +276,82 @@ if scan_sc_bb_bb_sc_torsions == True:
   pyplot.show()
   pyplot.close()
 
-file_name = "dQ_for_variable_bb_bb_bb_bb_torsion_angle.png"
-figure = pyplot.figure(1)
+if calculate_dQ:
+ file_name = "dQ_for_variable_bb_bb_bb_bb_torsion_angle.png"
+ figure = pyplot.figure(1)
 
-x=np.array([float(angle*(180.0/3.14)) for angle in bb_bb_bb_bb_equil_torsion_angles])
-y=np.array([float(dQ) for dQ in dQ_list])
+ x=np.array([float(angle*(180.0/3.14)) for angle in bb_bb_bb_bb_equil_torsion_angles])
+ y=np.array([float(dQ) for dQ in dQ_list])
 
-pyplot.xlabel(r"$ \alpha_{0}^{BB-BB-BB-BB} $ ( Degrees )")
-pyplot.ylabel(r"$\Delta$Q")
-pyplot.title(r"$\Delta$Q (Change in native contacts) during folding")
-pyplot.plot(x,y)
-pyplot.savefig(file_name)
-pyplot.show()
-pyplot.close()
+ pyplot.xlabel(r"$ \alpha_{0}^{BB-BB-BB-BB} $ ( Degrees )")
+ pyplot.ylabel(r"$\Delta$Q")
+ pyplot.title(r"$\Delta$Q (Change in native contacts) during folding")
+ pyplot.plot(x,y)
+ pyplot.savefig(file_name)
+ pyplot.show()
+ pyplot.close()
 
-  
+if calculate_free_energies:
+ file_name = "free_energies_for_variable_bb_bb_bb_bb_torsion_angle.png"
+ figure = pyplot.figure(1)
+ legend_title=r"$ \alpha_{0}^{BB-BB-BB-BB} $ (Degrees)"
+ legend_labels=np.array([float(round(angle*(180.0/3.14),1)) for angle in bb_bb_bb_bb_equil_torsion_angles])
+ temperatures=np.array([temperature for temperature in new_temp_list])
+ index = 0
+ for df_ij,ddf_ij in zip(df_ij_list,ddf_ij_list):
+   df_ij = np.array([df_ij[i][0] for i in range(len(df_ij))])
+   ddf_ij = np.array([ddf_ij[i][0] for i in range(len(ddf_ij))])
+   line, = pyplot.plot(temperatures,df_ij)
+   line.set_label(legend_labels[index])
+   index = index + 1
+
+ pyplot.xlabel("Temperature (Kelvin)")
+ pyplot.ylabel(r"Dimensionless free energy differences $\mathit{F}$")
+ pyplot.title(r"$\mathit{F}$ for variable $\alpha_{0}^{BB-BB-BB-BB}$")
+ pyplot.legend(legend_labels)
+ pyplot.savefig(file_name)
+ pyplot.show()
+ pyplot.close() 
+
+ file_name = "entropies_for_variable_bb_bb_bb_bb_torsion_angle.png"
+ figure = pyplot.figure(1)
+ legend_title=r"$ \alpha_{0}^{BB-BB-BB-BB} $ (Degrees)"
+ legend_labels=np.array([float(round(angle*(180.0/3.14),1)) for angle in bb_bb_bb_bb_equil_torsion_angles])
+ temperatures=np.array([temperature for temperature in new_temp_list])
+ index = 0
+ for Delta_s in Delta_s_list:
+   delta_s = np.array([Delta_s[i][0] for i in range(len(Delta_s))])
+   line, = pyplot.plot(temperatures,delta_s)
+   line.set_label(legend_labels[index])
+   index = index + 1
+
+ pyplot.xlabel("Temperature (Kelvin)")
+ pyplot.ylabel("Entropy differences ($\Delta$S)")
+ pyplot.title(r"Entropy for variable $\alpha_{0}^{BB-BB-BB-BB}$")
+ pyplot.legend(legend_labels)
+ pyplot.savefig(file_name)
+ pyplot.show()
+ pyplot.close()
+
+if evaluate_heat_capacity:
+ file_name = "heat_capacity_for_variable_bb_bb_bb_bb_torsion_angle.png"
+ figure = pyplot.figure(1)
+ legend_title=r"$ \alpha_{0}^{BB-BB-BB-BB} $ (Degrees)"
+ legend_labels=np.array([float(round(angle*(180.0/3.14),1)) for angle in bb_bb_bb_bb_equil_torsion_angles])
+ temperatures=np.array([temperature for temperature in new_temp_list])
+ index = 0
+ for C_v,dC_v in zip(C_v_list,dC_v_list):
+  C_v = np.array([C_v[i] for i in range(len(C_v))])
+  dC_v = np.array([dC_v[i] for i in range(len(dC_v))])
+  pyplot.errorbar(temperatures,C_v,yerr=dC_v,figure=figure,label=legend_labels[index])
+  index = index + 1
+
+ pyplot.xlabel("Temperature ( Kelvin )")
+ pyplot.ylabel(r"C$_{v}$ ( kcal/mol * Kelvin )")
+ pyplot.title(r"Heat capacity for variable $\epsilon$")
+ pyplot.legend(legend_labels)
+ pyplot.savefig(file_name)
+ pyplot.show()
+ pyplot.close()
 
 exit()
