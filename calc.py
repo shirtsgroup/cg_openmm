a2f67818657ebfe8b265476d15b8d77b59ce87a8
Michael Shirts <mrshirts@gmail.com>
2020-05-02 12:49:41 -0600

    Reformatted with black -l 99
    Former-commit-id: 1a52fcd9be40f7a2b8f8cfbccf8141702b9b8280 [formerly 6df151a5c0536fdbfe20d0f182167e80ca9bdf9a]
    Former-commit-id: 08c02b85554c84cbd80f20f4fb49590911645695
    Former-commit-id: 6a935d71aafca9f8b6713d3d5dcb4c2854c45270 [formerly 698b986b4e0930de11a387eed69cd3e2fb929278]
    Former-commit-id: f47c4014a945eb5c224952e9ad04b2b2faf4e3c3
    Former-commit-id: b8b39c8fc71be124828c3ff512495f33418fcf8f [formerly 67f04fd3209bf14a593897fc5084577fda2935e5]
    Former-commit-id: 8f53ea1e5cc199848a73fb8ea3eecfbca9622213

--- calc.py
+++ calc.py
@@ -2,10 +2,11 @@ import numpy as np
 from foldamers.parameters.reweight import *
 import matplotlib.pyplot as pyplot
 
-kB = 0.008314462  #Boltzmann constant (Gas constant) in kJ/(mol*K)
+kB = 0.008314462  # Boltzmann constant (Gas constant) in kJ/(mol*K)
 
-def plot_heat_capacity(C_v,dC_v,temperature_list,file_name="heat_capacity.png"):
-        """
+
+def plot_heat_capacity(C_v, dC_v, temperature_list, file_name="heat_capacity.png"):
+    """
         Given an array of temperature-dependent heat capacity values and the uncertainties in their estimates, this function plots the heat capacity curve.
 
         :param C_v: The heat capacity data to plot.
@@ -18,20 +19,21 @@ def plot_heat_capacity(C_v,dC_v,temperature_list,file_name="heat_capacity.png"):
         :type file_name: str
 
         """
-        figure = pyplot.figure(1)
-        temperature_list = np.array([temperature for temperature in temperature_list])
-        C_v = np.array([C_v[i] for i in range(len(C_v))])
-        dC_v = np.array([dC_v[i] for i in range(len(dC_v))])
-        pyplot.errorbar(temperature_list,C_v,yerr=dC_v,figure=figure)
-        pyplot.xlabel("Temperature ( Kelvin )")
-        pyplot.ylabel("C$_v$ ( kcal/mol * Kelvin )")
-        pyplot.title("Heat capacity for")
-        pyplot.savefig(file_name)
-        pyplot.close()
-        return
+    figure = pyplot.figure(1)
+    temperature_list = np.array([temperature for temperature in temperature_list])
+    C_v = np.array([C_v[i] for i in range(len(C_v))])
+    dC_v = np.array([dC_v[i] for i in range(len(dC_v))])
+    pyplot.errorbar(temperature_list, C_v, yerr=dC_v, figure=figure)
+    pyplot.xlabel("Temperature ( Kelvin )")
+    pyplot.ylabel("C$_v$ ( kcal/mol * Kelvin )")
+    pyplot.title("Heat capacity for")
+    pyplot.savefig(file_name)
+    pyplot.close()
+    return
 
-def get_heat_capacity(replica_energies,temperature_list,num_intermediate_states=None):
-        """
+
+def get_heat_capacity(replica_energies, temperature_list, num_intermediate_states=None):
+    """
         Given a set of trajectories, a temperature list, and a number of intermediate states to insert for the temperature list, this function calculates and plots the heat capacity profile.
 
         :param replica_energies: List of dimension num_replicas X simulation_steps, which gives the energies for all replicas at all simulation steps 
@@ -49,102 +51,161 @@ def get_heat_capacity(replica_energies,temperature_list,num_intermediate_states=
           - new_temp_list ( List( float * unit.simtk.temperature ) ) - The temperature list corresponding to the heat capacity values in 'C_v'
 
         """
-        if num_intermediate_states == None:
-         num_intermediate_states = 1
-        mbar,E_kn,E_expect,dE_expect,new_temp_list = get_mbar_expectation(replica_energies,temperature_list,num_intermediate_states)
-        mbar,E_kn,DeltaE_expect,dDeltaE_expect,new_temp_list = get_mbar_expectation(E_kn,new_temp_list,num_intermediate_states,mbar=mbar,output='differences')
-        mbar,E_kn,E2_expect,dE2_expect,new_temp_list = get_mbar_expectation(E_kn**2,new_temp_list,num_intermediate_states,mbar=mbar)
-        df_ij,ddf_ij = get_free_energy_differences(mbar)
-        C_v,dC_v = calculate_heat_capacity(E_expect,E2_expect,dE_expect,DeltaE_expect,dDeltaE_expect,df_ij,ddf_ij,new_temp_list,len(temperature_list),num_intermediate_states)
-        plot_heat_capacity(C_v,dC_v,new_temp_list)
-        return(C_v,dC_v,new_temp_list)
+    if num_intermediate_states == None:
+        num_intermediate_states = 1
+    mbar, E_kn, E_expect, dE_expect, new_temp_list = get_mbar_expectation(
+        replica_energies, temperature_list, num_intermediate_states
+    )
+    mbar, E_kn, DeltaE_expect, dDeltaE_expect, new_temp_list = get_mbar_expectation(
+        E_kn, new_temp_list, num_intermediate_states, mbar=mbar, output="differences"
+    )
+    mbar, E_kn, E2_expect, dE2_expect, new_temp_list = get_mbar_expectation(
+        E_kn ** 2, new_temp_list, num_intermediate_states, mbar=mbar
+    )
+    df_ij, ddf_ij = get_free_energy_differences(mbar)
+    C_v, dC_v = calculate_heat_capacity(
+        E_expect,
+        E2_expect,
+        dE_expect,
+        DeltaE_expect,
+        dDeltaE_expect,
+        df_ij,
+        ddf_ij,
+        new_temp_list,
+        len(temperature_list),
+        num_intermediate_states,
+    )
+    plot_heat_capacity(C_v, dC_v, new_temp_list)
+    return (C_v, dC_v, new_temp_list)
 
-def calculate_heat_capacity(E_expect,E2_expect,dE_expect,DeltaE_expect,dDeltaE_expect,df_ij,ddf_ij,Temp_k,originalK,numIntermediates,ntypes=3,dertype="temperature"):
+
+def calculate_heat_capacity(
+    E_expect,
+    E2_expect,
+    dE_expect,
+    DeltaE_expect,
+    dDeltaE_expect,
+    df_ij,
+    ddf_ij,
+    Temp_k,
+    originalK,
+    numIntermediates,
+    ntypes=3,
+    dertype="temperature",
+):
     """
     Given numerous pieces of thermodynamic data this function calculates the heat capacity by following the `'pymbar' example <https://github.com/choderalab/pymbar/tree/master/examples/heat-capacity>`_ .
 
     """
 
-    #------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
     # Compute Cv for NVT simulations as <E^2> - <E>^2 / (RT^2)
-    #------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
 
-    #print("")
-    #print("Computing Heat Capacity as ( <E^2> - <E>^2 ) / ( R*T^2 ) and as d<E>/dT")
+    # print("")
+    # print("Computing Heat Capacity as ( <E^2> - <E>^2 ) / ( R*T^2 ) and as d<E>/dT")
 
     K = len(Temp_k)
 
-    allCv_expect = np.zeros([K,ntypes], np.float64)
-    dCv_expect = np.zeros([K,ntypes],np.float64)
+    allCv_expect = np.zeros([K, ntypes], np.float64)
+    dCv_expect = np.zeros([K, ntypes], np.float64)
     try:
-      Temp_k = np.array([temp._value for temp in Temp_k])
+        Temp_k = np.array([temp._value for temp in Temp_k])
     except:
-      Temp_k = np.array([temp for temp in Temp_k])
+        Temp_k = np.array([temp for temp in Temp_k])
 
-
-    allCv_expect[:,0] = (E2_expect - (E_expect*E_expect)) / ( kB * Temp_k**2)
+    allCv_expect[:, 0] = (E2_expect - (E_expect * E_expect)) / (kB * Temp_k ** 2)
 
     ####################################
     # C_v by fluctuation formula
     ####################################
 
-    N_eff = (E2_expect - (E_expect*E_expect))/dE_expect**2  # sigma^2 / (sigma^2/n) = effective number of samples
-    dCv_expect[:,0] = allCv_expect[:,0]*np.sqrt(2/N_eff)
+    N_eff = (
+        E2_expect - (E_expect * E_expect)
+    ) / dE_expect ** 2  # sigma^2 / (sigma^2/n) = effective number of samples
+    dCv_expect[:, 0] = allCv_expect[:, 0] * np.sqrt(2 / N_eff)
 
-    for i in range(originalK,K):
+    for i in range(originalK, K):
 
         # Now, calculae heat capacity by T-differences
-        im = i-1
-        ip = i+1
-        #print(im,ip)
-        if (i==originalK):
+        im = i - 1
+        ip = i + 1
+        # print(im,ip)
+        if i == originalK:
             im = originalK
-        if (i==K-1):
+        if i == K - 1:
             ip = i
         ####################################
         # C_v by first derivative of energy
         ####################################
 
-        if (dertype == 'temperature'):  # temperature derivative
+        if dertype == "temperature":  # temperature derivative
             # C_v = d<E>/dT
-            allCv_expect[i,1] = (DeltaE_expect[im,ip])/(Temp_k[ip]-Temp_k[im])
-            dCv_expect[i,1] = (dDeltaE_expect[im,ip])/(Temp_k[ip]-Temp_k[im])
-        elif (dertype == 'beta'):  # beta derivative
-            #Cv = d<E>/dT = dbeta/dT d<E>/beta = - kB*T(-2) d<E>/dbeta  = - kB beta^2 d<E>/dbeta
-            allCv_expect[i,1] = kB * beta_k[i]**2 * (DeltaE_expect[ip,im])/(beta_k[ip]-beta_k[im])
-            dCv_expect[i,1] = -kB * beta_k[i]**2 *(dDeltaE_expect[ip,im])/(beta_k[ip]-beta_k[im])
+            allCv_expect[i, 1] = (DeltaE_expect[im, ip]) / (Temp_k[ip] - Temp_k[im])
+            dCv_expect[i, 1] = (dDeltaE_expect[im, ip]) / (Temp_k[ip] - Temp_k[im])
+        elif dertype == "beta":  # beta derivative
+            # Cv = d<E>/dT = dbeta/dT d<E>/beta = - kB*T(-2) d<E>/dbeta  = - kB beta^2 d<E>/dbeta
+            allCv_expect[i, 1] = (
+                kB * beta_k[i] ** 2 * (DeltaE_expect[ip, im]) / (beta_k[ip] - beta_k[im])
+            )
+            dCv_expect[i, 1] = (
+                -kB * beta_k[i] ** 2 * (dDeltaE_expect[ip, im]) / (beta_k[ip] - beta_k[im])
+            )
 
         ####################################
         # C_v by second derivative of free energy
         ####################################
 
-        if (dertype == 'temperature'):
+        if dertype == "temperature":
             # C_v = d<E>/dT = d/dT k_B T^2 df/dT = 2*T*df/dT + T^2*d^2f/dT^2
 
-            if (i==originalK) or (i==K-1):
-                 # We can't calculate this, set a number that will be printed as NAN
-                 allCv_expect[i,2] = -10000000.0
+            if (i == originalK) or (i == K - 1):
+                # We can't calculate this, set a number that will be printed as NAN
+                allCv_expect[i, 2] = -10000000.0
             else:
-                allCv_expect[i,2] = kB*Temp_k[i]*(2*df_ij[ip,im]/(Temp_k[ip]-Temp_k[im]) +
-                                                    Temp_k[i]*(df_ij[ip,i]-df_ij[i,im])/
-                                                    ((Temp_k[ip]-Temp_k[im])/(ip-im))**2)
+                allCv_expect[i, 2] = (
+                    kB
+                    * Temp_k[i]
+                    * (
+                        2 * df_ij[ip, im] / (Temp_k[ip] - Temp_k[im])
+                        + Temp_k[i]
+                        * (df_ij[ip, i] - df_ij[i, im])
+                        / ((Temp_k[ip] - Temp_k[im]) / (ip - im)) ** 2
+                    )
+                )
 
-                A = 2*Temp_k[i]/(Temp_k[ip]-Temp_k[im]) + 4*Temp_k[i]**2/(Temp_k[ip]-Temp_k[im])**2
-                B = 2*Temp_k[i]/(Temp_k[ip]-Temp_k[im]) + 4*Temp_k[i]**2/(Temp_k[ip]-Temp_k[im])**2
-                #dCv_expect[i,2,n] = kB* [(A ddf_ij[ip,i])**2 + (B sdf_ij[i,im])**2 + 2*A*B*cov(df_ij[ip,i],df_ij[i,im])
+                A = (
+                    2 * Temp_k[i] / (Temp_k[ip] - Temp_k[im])
+                    + 4 * Temp_k[i] ** 2 / (Temp_k[ip] - Temp_k[im]) ** 2
+                )
+                B = (
+                    2 * Temp_k[i] / (Temp_k[ip] - Temp_k[im])
+                    + 4 * Temp_k[i] ** 2 / (Temp_k[ip] - Temp_k[im]) ** 2
+                )
+                # dCv_expect[i,2,n] = kB* [(A ddf_ij[ip,i])**2 + (B sdf_ij[i,im])**2 + 2*A*B*cov(df_ij[ip,i],df_ij[i,im])
                 # This isn't it either: need to figure out that last term.
-                dCv_expect[i,2] = kB*((A*ddf_ij[ip,i])**2 + (B*ddf_ij[i,im])**2)
+                dCv_expect[i, 2] = kB * ((A * ddf_ij[ip, i]) ** 2 + (B * ddf_ij[i, im]) ** 2)
                 # Would need to add function computing covariance of DDG, (A-B)-(C-D)
 
-        elif (dertype == 'beta'):
+        elif dertype == "beta":
             # if beta is evenly spaced, rather than t, we can do 2nd derivative in beta
             # C_v = d<E>/dT = d/dT (df/dbeta) = dbeta/dT d/dbeta (df/dbeta) = -k_b beta^2 df^2/d^2beta
-            if (i==originalK) or (i==K-1):
-                #Flag as N/A -- we don't try to compute at the endpoints for now
-                allCv_expect[i,2] = -10000000.0
+            if (i == originalK) or (i == K - 1):
+                # Flag as N/A -- we don't try to compute at the endpoints for now
+                allCv_expect[i, 2] = -10000000.0
             else:
-                allCv_expect[i,2] = kB * beta_k[i]**2 *(df_ij[ip,i]-df_ij[i,im])/((beta_k[ip]-beta_k[im])/(ip-im))**2
-            dCv_expect[i,2] = kB*(beta_k[i])**2 * (ddf_ij[ip,i]-ddf_ij[i,im])/((beta_k[ip]-beta_k[im])/(ip-im))**2
-                # also wrong, need to be fixed.
-   
-    return(allCv_expect[:,0],dCv_expect[:,0])
+                allCv_expect[i, 2] = (
+                    kB
+                    * beta_k[i] ** 2
+                    * (df_ij[ip, i] - df_ij[i, im])
+                    / ((beta_k[ip] - beta_k[im]) / (ip - im)) ** 2
+                )
+            dCv_expect[i, 2] = (
+                kB
+                * (beta_k[i]) ** 2
+                * (ddf_ij[ip, i] - ddf_ij[i, im])
+                / ((beta_k[ip] - beta_k[im]) / (ip - im)) ** 2
+            )
+            # also wrong, need to be fixed.
+
+    return (allCv_expect[:, 0], dCv_expect[:, 0])
