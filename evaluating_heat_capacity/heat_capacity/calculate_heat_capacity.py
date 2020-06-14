91c0a641c09d86076633531e929079a6355f293e
Michael Shirts <mrshirts@gmail.com>
2020-06-14 14:36:56 -0600

    Add heat capacity.

---
+++ evaluating_heat_capacity/heat_capacity/calculate_heat_capacity.py
@@ -0,0 +1,18 @@
+import os
+import numpy as np
+import matplotlib.pyplot as pyplot
+from simtk import unit
+from foldamers.cg_model.cgmodel import CGModel
+from foldamers.thermo.calc import *
+from foldamers.parameters.reweight import get_temperature_list
+
+# Replica exchange simulation settings
+number_replicas = 12
+min_temp = 50 * unit.kelvin
+max_temp = 600.0 * unit.kelvin
+temperature_list = get_temperature_list(min_temp, max_temp, number_replicas)
+
+# Read the simulation coordinates for individual temperature replicas                                                                     
+C_v, dC_v, new_temperature_list = get_heat_capacity(temperature_list, output_data="output.nc",
+                                                    output_directory="output", num_intermediate_states=5)
+
