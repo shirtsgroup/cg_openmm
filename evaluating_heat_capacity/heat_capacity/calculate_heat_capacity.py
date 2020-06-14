b6e8d268e89d116c321698de6617ba7506f7470b
Michael Shirts <mrshirts@gmail.com>
2020-06-14 14:38:11 -0600

    more explanations.

--- evaluating_heat_capacity/heat_capacity/calculate_heat_capacity.py
+++ evaluating_heat_capacity/heat_capacity/calculate_heat_capacity.py
@@ -6,7 +6,8 @@ from foldamers.cg_model.cgmodel import CGModel
 from foldamers.thermo.calc import *
 from foldamers.parameters.reweight import get_temperature_list
 
-# Replica exchange simulation settings
+# Replica exchange simulation settings.
+#These must match the simulations that are being analyzed.
 number_replicas = 12
 min_temp = 50 * unit.kelvin
 max_temp = 600.0 * unit.kelvin
@@ -15,4 +16,3 @@ temperature_list = get_temperature_list(min_temp, max_temp, number_replicas)
 # Read the simulation coordinates for individual temperature replicas                                                                     
 C_v, dC_v, new_temperature_list = get_heat_capacity(temperature_list, output_data="output.nc",
                                                     output_directory="output", num_intermediate_states=5)
-
