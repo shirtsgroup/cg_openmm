6eaa78e0efc81fe0f7ecd5814bc3529a215c95f5
Michael Shirts <mrshirts@gmail.com>
2020-06-17 17:24:56 -0600

    Cleaning up heat capacity folder.

--- evaluating_heat_capacity/heat_capacity/calculate_heat_capacity.py
+++ evaluating_heat_capacity/heat_capacity/calculate_heat_capacity.py
@@ -15,4 +15,7 @@ temperature_list = get_temperature_list(min_temp, max_temp, number_replicas)
 
 # Read the simulation coordinates for individual temperature replicas                                                                     
 C_v, dC_v, new_temperature_list = get_heat_capacity(temperature_list, output_data="output.nc",
-                                                    output_directory="output", num_intermediate_states=5)
+                                                    output_directory="output", num_intermediate_states=3,plot_file='heat_capacity.pdf')
+print(f"T({new_temperature_list[0].unit})  Cv({C_v[0].unit})  dCv({dC_v[0].unit})")
+for i, C in enumerate(C_v):
+    print(f"{new_temperature_list[i]._value:>8.2f}{C_v[i]._value:>10.4f} {dC_v[i]._value:>10.4f}")
