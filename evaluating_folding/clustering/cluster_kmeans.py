cde418d2a64f9574ffe6d4820780df1507020ce0
Christopher Walker <Christopher.Walker-1@colorado.edu>
2020-06-19 08:57:44 -0600

    adding output of cluster sizes and rmsds

--- evaluating_folding/clustering/cluster_kmeans.py
+++ evaluating_folding/clustering/cluster_kmeans.py
@@ -109,13 +109,13 @@ for i in range(number_replicas):
     pdb_file_list.append("output/replica_%s.pdb" %(i+1))
 
 # Set clustering parameters
-n_clusters=2
+n_clusters=4
 frame_start=0
 frame_stride=10
 frame_end=-1
 
 # Run KMeans clustering
-centroid_positions, medoid_positions = get_cluster_centroid_positions(
+centroid_positions, medoid_positions, cluster_size, cluster_rmsd = get_cluster_centroid_positions(
     pdb_file_list=pdb_file_list,
     cgmodel=cgmodel,
     n_clusters=n_clusters,
@@ -123,3 +123,7 @@ centroid_positions, medoid_positions = get_cluster_centroid_positions(
     frame_stride=frame_stride,
     frame_end=-1)
 
+print('\nCluster sizes:')
+print(cluster_size)
+print('\nCluster rmsd:')
+print(cluster_rmsd)
