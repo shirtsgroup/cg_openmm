28763450a1b11fa0283544aec1e61ae938b2d301
Christopher Walker <Christopher.Walker-1@colorado.edu>
2020-07-10 09:44:35 -0600

    added new function for computing and plotting bond angle and torsion distributions, with example

---
+++ evaluating_parameter_distributions/new_angle_distributions/calc_angle_distributions.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+import matplotlib.pyplot as pyplot
+import mdtraj as md
+from simtk import unit
+from foldamers.cg_model.cgmodel import CGModel
+import pickle
+from foldamers.parameters.angle_distributions import *
+
+# This script calculates and plots angle distributions from a CGModel object and pdb trajectory
+
+# Load in a trajectory pdb file:
+traj_file = "24mer_1b1s_traj_166K.pdb"
+
+# Load in a CGModel:
+cgmodel = pickle.load(open("stored_cgmodel.pkl","rb"))
+
+angle_hist_data = calc_bond_angle_distribution(cgmodel,traj_file)
+
+torsion_hist_data = calc_torsion_distribution(cgmodel,traj_file)
