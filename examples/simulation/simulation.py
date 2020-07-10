0cba0d95d9a37e18763318d88433f45044aae5ee
Michael Shirts <mrshirts@gmail.com>
2020-07-10 09:25:10 -0600

    update simulation script for new format.

--- examples/simulation/simulation.py
+++ examples/simulation/simulation.py
@@ -24,80 +24,72 @@ if not os.path.exists(output_directory):
 print_frequency = 5000  # Number of steps to skip when printing output
 total_simulation_time = 2.0 * unit.nanosecond  # Units = picoseconds
 simulation_time_step = 5.0 * unit.femtosecond
-total_steps = int(np.floor(total_simulation_time/simulation_time_step))
+total_steps = int(np.floor(total_simulation_time / simulation_time_step))
 temperature = 300.0 * unit.kelvin
 friction = 1.0 / unit.picosecond
 
-# Coarse grained model settings
-polymer_length = 12
-backbone_lengths = [1]
-sidechain_lengths = [1]
-sidechain_positions = [0]
+
 include_bond_forces = True
 include_bond_angle_forces = True
 include_nonbonded_forces = True
 include_torsion_forces = True
 constrain_bonds = False
 
-# Bond definitions
-bond_length = 0.2 * unit.nanometer
-bond_lengths = {
-    "bb_bb_bond_length": bond_length,
-    "bb_sc_bond_length": bond_length,
-    "sc_sc_bond_length": bond_length,
-}
-bond_force_constant = 1000 * unit.kilojoule_per_mole / unit.nanometer / unit.nanometer
-bond_force_constants = {
-    "bb_bb_bond_k": bond_force_constant,
-    "bb_sc_bond_k": bond_force_constant,
-    "sc_sc_bond_k": bond_force_constant,
-}
+bond_length = 0.2 * unit.nanometer  # reference length unit
 
 # Particle definitions
-mass = 100.0 * unit.amu
-masses = {"backbone_bead_masses": mass, "sidechain_bead_masses": mass}
 r_min = 1.5 * bond_length  # Lennard-Jones potential r_min
 # Factor of /(2.0**(1/6)) is applied to convert r_min to sigma
 sigma = r_min / (2.0 ** (1.0 / 6.0))
-sigmas = {"bb_sigma": sigma, "sc_sigma": sigma}
-epsilon = 0.5 * unit.kilojoule_per_mole
-epsilons = {"bb_eps": epsilon, "sc_eps": epsilon}
+epsilon = 0.5 * unit.kilojoules_per_mole
+mass = 100.0 * unit.amu
+
+# mass and charge are defaults.
+bb = {"particle_type_name": "bb", "sigma": sigma, "epsilon": epsilon, "mass": mass}
+sc = {"particle_type_name": "sc", "sigma": sigma, "epsilon": epsilon, "mass": mass}
+
+# Monomer definition
+A = {
+    "monomer_name": "A",
+    "particle_sequence": [bb, sc],
+    "bond_list": [[0, 1]],
+    "start": 0,
+    "end": 0,
+}
+
+sequence = 12 * [A]
+
+# Bond definitions
+bond_lengths = {"default_bond_length": bond_length}
+
+bond_force_constants = {
+    "default_bond_force_constant": 1000 * unit.kilojoule_per_mole / unit.nanometer / unit.nanometer
+}
 
 # Bond angle definitions
-bond_angle_force_constant = 50.0 * unit.kilojoule_per_mole / unit.radian / unit.radian
 bond_angle_force_constants = {
-    "bb_bb_bb_angle_k": bond_angle_force_constant,
-    "bb_bb_sc_angle_k": bond_angle_force_constant,
-}
-bb_bb_bb_equil_bond_angle = 120.0 * unit.degrees
-bb_bb_sc_equil_bond_angle = 120.0 * unit.degrees
-equil_bond_angles = {
-    "bb_bb_bb_angle_0": bb_bb_bb_equil_bond_angle,
-    "bb_bb_sc_angle_0": bb_bb_sc_equil_bond_angle,
+    "default_bond_angle_force_constant": 50.0 * unit.kilojoule_per_mole / unit.radian / unit.radian
 }
 
-# Torsion angle definitions
-torsion_force_constant = 20.0 * unit.kilojoule_per_mole
-torsion_force_constants = {"bb_bb_bb_bb_torsion_k": torsion_force_constant}
-bb_bb_bb_bb_equil_torsion_angle = 78.0 * unit.degrees
-bb_bb_bb_sc_equil_torsion_angle = 78.0 * unit.degrees
-equil_torsion_angles = {"bb_bb_bb_bb_torsion_0": bb_bb_bb_bb_equil_torsion_angle,
-                        "bb_bb_bb_sc_torsion_0": bb_bb_bb_sc_equil_torsion_angle
+equil_bond_angles = {"default_equil_bond_angle": 120.0 * unit.degrees}
+
+# torsion angle definitions
+torsion_force_constants = {"bb_bb_bb_bb_torsion_k": 20.0 * unit.kilojoule_per_mole}
+
+torsion_angle = 78.0 * unit.degrees
+equil_torsion_angles = {
+    "bb_bb_bb_bb_torsion_0": torsion_angle,
+    "bb_bb_bb_sc_torsion_0": torsion_angle,
 }
-torsion_periodicities = {"bb_bb_bb_bb_period": 3, "bb_bb_bb_sc_period":3}
+
+torsion_periodicities = {"bb_bb_bb_bb_period": 3, "bb_bb_bb_sc_period": 3}
 
 # Get initial positions from local file
 positions = PDBFile("helix2.pdb").getPositions()
 
 # Build a coarse grained model
 cgmodel = CGModel(
-    polymer_length=polymer_length,
-    backbone_lengths=backbone_lengths,
-    sidechain_lengths=sidechain_lengths,
-    sidechain_positions=sidechain_positions,
-    masses=masses,
-    sigmas=sigmas,
-    epsilons=epsilons,
+    particle_type_list=[bb, sc],
     bond_lengths=bond_lengths,
     bond_force_constants=bond_force_constants,
     bond_angle_force_constants=bond_angle_force_constants,
@@ -111,6 +103,8 @@ cgmodel = CGModel(
     include_torsion_forces=include_torsion_forces,
     constrain_bonds=constrain_bonds,
     positions=positions,
+    sequence=sequence,
+    monomer_types=[A],
 )
 
 # Run a simulation
