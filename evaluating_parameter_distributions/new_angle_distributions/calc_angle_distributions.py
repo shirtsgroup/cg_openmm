b50b2d869d75f223dd45c031420f4147c7ddb183
Christopher Walker <Christopher.Walker-1@colorado.edu>
2020-07-29 14:46:19 -0600

    cleaning up a bunch of files

--- evaluating_parameter_distributions/new_angle_distributions/calc_angle_distributions.py
+++ evaluating_parameter_distributions/new_angle_distributions/calc_angle_distributions.py
@@ -10,11 +10,11 @@ from foldamers.parameters.angle_distributions import *
 # This script calculates and plots angle distributions from a CGModel object and pdb trajectory
 
 # Load in a trajectory pdb file:
-traj_file = "24mer_1b1s_traj_166K.pdb"
+traj_file = "simulation.pdb"
 
 # Load in a CGModel:
 cgmodel = pickle.load(open("stored_cgmodel.pkl","rb"))
 
 angle_hist_data = calc_bond_angle_distribution(cgmodel,traj_file)
-
 torsion_hist_data = calc_torsion_distribution(cgmodel,traj_file)
+
