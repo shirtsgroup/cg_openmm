b448361978ab585933411273ce220a56807f046f
Chris Walker <Christopher.Walker-1@colorado.edu>
2020-10-02 08:55:16 -0600

    adding a sigmoid fitting tool for Q vs T

--- cg_openmm/utilities/util.py
+++ cg_openmm/utilities/util.py
@@ -4,6 +4,8 @@ from simtk.openmm import *
 from simtk import unit
 import simtk.openmm.app.element as elem
 from simtk.openmm.app import *
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 
 
 def distance(positions_1, positions_2):
@@ -117,3 +119,98 @@ def lj_v(positions_1, positions_2, sigma, epsilon):
     rep = quot.__pow__(12.0)
     v = 4.0 * epsilon.__mul__(rep.__sub__(attr))
     return v
+    
+    
+def fit_sigmoid(xdata, ydata, plotfile='Q_vs_T_fit.pdf', xlabel='T (K)', ylabel='Q'):
+    """
+    Fit a sigmoidal curve (such as native contact fraction vs T) to hyperbolic tangent switching function
+    
+    :param xdata: x data series
+    :type xdata: Quantity or numpy 1D array
+    
+    :param ydata: y data series
+    :type ydata: Quantity or numpy 1D array
+
+    """
+    
+    # Strip units off quantities:
+    if type(xdata[0]) == unit.quantity.Quantity:
+        xdata_val = np.zeros((len(xdata)))
+        xunit = xdata[0].unit
+        for i in range(len(xdata)):
+            xdata_val[i] = xdata[i].value_in_unit(xunit)
+        xdata = xdata_val
+    
+    if type(ydata[0]) == unit.quantity.Quantity:
+        ydata_val = np.zeros((len(ydata)))
+        yunit = ydata[0].unit
+        for i in range(len(ydata)):
+            ydata_val[i] = ydata[i].value_in_unit(yunit)
+        ydata = ydata_val
+        
+    
+    def tanh_switch(x,x0,y0,y1,d):
+        return (y0+y1)/2-((y0-y1)/2)*np.tanh(np.radians(x-x0)/d)
+        
+    param_guess = [np.mean(xdata),0.15,0.85,100]
+    
+    param_opt, param_cov = curve_fit(tanh_switch, xdata, ydata, param_guess)
+    
+    figure = plt.figure()
+    
+    xfit = np.linspace(np.min(xdata),np.max(xdata),1000)
+    yfit = tanh_switch(xfit,param_opt[0],param_opt[1],param_opt[2],param_opt[3])
+    
+    # Value of y at the critical value of x:
+    y_x0 = (param_opt[1]+param_opt[2])/2
+    y_x0_err = np.sqrt(np.power(param_cov[1,1],2)+np.power(param_cov[2,2],2))
+    
+    line1 = plt.plot(
+        xdata,
+        ydata,
+        'ok',
+        markersize=4,
+        fillstyle='none',
+        label='simulation data',
+    )
+    
+    line2 = plt.plot(
+        xfit,
+        yfit,
+        '-b',
+        label='hyperbolic fit',
+    )
+    
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    
+    plt.legend()
+    
+    xlim = plt.xlim()
+    ylim = plt.ylim()
+    
+    plt.text(
+        ((xlim[0]+xlim[1])/2),
+        (ylim[0]+0.75*(ylim[1]-ylim[0])),
+        r'$y(x)=\frac{y0+y1}{2}-\frac{y0-y1}{2}\left(\tanh\left(\frac{(x-x0)}{d}\right)\right)$',
+    )
+    
+    plt.text(
+        (xlim[0]+0.55*(xlim[1]-xlim[0])),
+        (ylim[0]+0.50*(ylim[1]-ylim[0])),
+        f'x0 = {param_opt[0]:.4e} \u00B1 {param_cov[0,0]:.4e}\n' \
+        f'y0 = {param_opt[1]:.4e} \u00B1 {param_cov[1,1]:.4e}\n' \
+        f'y1 = {param_opt[2]:.4e} \u00B1 {param_cov[2,2]:.4e}\n' \
+        f'd = {param_opt[3]:.4e} \u00B1 {param_cov[3,3]:.4e}\n' \
+        f'y(x0) = {y_x0:>.4e} \u00B1 {y_x0_err:.4e}',
+        {'fontsize': 8},
+        horizontalalignment='left',
+    )
+    
+    plt.savefig(plotfile)
+    plt.close()
+    
+    return param_opt, param_cov
+    
+    
+    
